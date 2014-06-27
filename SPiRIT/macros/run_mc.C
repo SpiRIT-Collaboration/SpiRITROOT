998dad6c132f9d22379485f836318af930ada48e
Mike <mdyoungs@comp.tamu.edu>
2014-06-27 15:43:13 +0900

    Added media file as well as program to run Monte Carlo through GEANT

--- SPiRIT/macros/run_mc.C
+++ SPiRIT/macros/run_mc.C
@@ -1,4 +1,11 @@
-void run_mc(Int_t nEvents = 10)
+//#include "../../fairtools/FairLogger.h"
+//#include "FairRunSim.h"
+#include <Riostream.h>
+#include <TVector3.h>
+#include <TMath.h>
+#include <TLorentzVector.h>
+
+void run_mc(const Int_t nEvents = 10)
 {
 
   gRandom -> SetSeed(time(0));
@@ -13,8 +20,114 @@ void run_mc(Int_t nEvents = 10)
   gSystem->Setenv("CONFIG_DIR",tut_configdir.Data());
 
   TString outDir  = "data";
-  TString outFile = outDir + "/test.mc.root";
-  TString parFile = outDir + "/params.root";
+
+  //masses all are in MeV/c^2
+  //There are two types: pions and nucleons/clusters
+  //pi[0]=pi0, pi[1]=pi+ and pi-
+  //nucleons are in mass[Z][A] format
+  //currently only a few of the nucleons/clusters are programmed
+  //will need a mass excess table for the rest
+  const Int_t maxz=2, maxa=4;
+  Double_t masspi[2];
+  Double_t mass[maxz+1][maxa+1];
+  masspi[0]=134.976;
+  masspi[1]=139.570;
+  Double_t amu2mev=931.4941;
+  ifstream masses("etable.dat");
+  Int_t ztemp, atemp;
+  for (Int_t line=0; line<92; line++) {
+    masses >> ztemp >> atemp;
+    if (ztemp<=maxz && atemp<=maxa) masses >> mass[ztemp][atemp];
+  }
+  for (Int_t z=0; z<=maxz; z++) {
+    for (Int_t a=0; a<=maxa; a++) {
+      if (mass[z][a]!=0.0) mass[z][a]=(a*1.0+mass[z][a]*1.0E-6)*amu2mev;
+      else mass[z][a]=a*1.0*amu2mev;
+    }
+  }
+
+  ifstream urqmd("132sn124sn300amev_urqmd_short.dat");
+  Int_t beamA, beamZ, targA, targZ, beamEperA;
+  const Int_t events;
+  urqmd >> beamA >> beamZ >> targA >> targZ >> beamEperA >> events;
+  Double_t beamE=1.0*beamEperA*beamA+beamA*amu2mev;
+  Double_t totalE=beamE+targA*amu2mev;
+  Double_t beamP=TMath::Sqrt(beamE*beamE-beamA*beamA*amu2mev*amu2mev);
+  Double_t mBeta=-beamP/totalE;
+  TVector3 beta(0.,0.,-mBeta);
+  TLorentzVector mLorentz;  
+
+  //UrQMD
+  vector<int> partz[events];
+  vector<int> parta[events];
+  vector<double> pxc[events];
+  vector<double> pyc[events];
+  vector<double> pzc[events];
+  vector<double> pxl[events];
+  vector<double> pyl[events];
+  vector<double> pzl[events];
+  vector<int> pid[events];
+  vector<double> kinl[events];
+
+  Int_t evtnum, evtmult;;
+  Double_t px, py, pz;
+  Int_t urA, urZ;
+  Double_t tempmass;
+  Double_t totalC, totalL;
+  Int_t pdg;
+
+  cout << "testhere1" << endl;
+
+  for (Int_t i=1; i<=5; i++) {
+    urqmd >> evtnum >> evtmult;
+    cout << evtnum << "\t" << evtmult << endl;
+    for (Int_t j=1; j<=evtmult; j++) {
+      urqmd >> urA >> urZ >> px >> py >> pz;
+      if (urA<=maxa && urZ<=maxz) {
+	//cout << urA << "\t" << urZ << "\t" << px << "\t" << py << "\t" << pz << endl;
+	partz[i-1].push_back(urZ);
+	parta[i-1].push_back(urA);
+	pxc[i-1].push_back(px);
+	pyc[i-1].push_back(py);
+	pzc[i-1].push_back(pz);
+	if (urA==-1) {
+	  if (urZ==0) tempmass=masspi[0];
+	  else tempmass=masspi[1];
+	}
+	else {
+	  tempmass=mass[urZ][urA];
+	}
+	totalC=TMath::Sqrt(px*px+py*py+pz*pz+tempmass*tempmass);
+	mLorentz.SetPxPyPzE(px,py,pz,totalC);
+	mLorentz.Boost(beta);
+	pxl[i-1].push_back(mLorentz.Px()/1000.);
+	pyl[i-1].push_back(mLorentz.Py()/1000.);
+	pzl[i-1].push_back(mLorentz.Pz()/1000.);
+	totalL=TMath::Sqrt(TMath::Power(mLorentz.Px(),2.)+TMath::Power(mLorentz.Py(),2.)+TMath::Power(mLorentz.Pz(),2.)+tempmass*tempmass);
+	kinl[i-1].push_back(totalL-tempmass);
+	//Setting pdg
+	if (urA==-1 && urZ==1) pdg=211;
+	if (urA==-1 && urZ==0) pdg=111;
+	if (urA==-1 && urZ==-1) pdg=-211;
+	if (urA==1 && urZ==0) pdg=2112;
+	if (urA==1 && urZ==1) pdg=2212;
+	if (urA==2 && urZ==1) pdg=1000010020;
+	if (urA==3 && urZ==1) pdg=1000010030;
+	if (urA==3 && urZ==2) pdg=1000020030;
+	if (urA==4 && urZ==2) pdg=1000020040;
+	pid[i-1].push_back(pdg);
+      }
+      //if (i==events && j==evtmult) urqmd.close();
+    }
+    
+    // if (i==events) {
+    //   for (Int_t k=0; k<pid[i].size(); k++) {
+    // 	cout << k+1 << "\t" << pid[i][k] << endl;
+    //   }
+    // }
+  }
+
+  cout << "testhere2" << endl;
 
   // In general, the following parts need not be touched
   // ========================================================================
@@ -28,107 +141,135 @@ void run_mc(Int_t nEvents = 10)
   timer.Start();
   // ------------------------------------------------------------------------
 
-   FairLogger *logger = FairLogger::GetLogger();
-  // define log file name
-  logger->SetLogFileName("MyLog.log");
+  FairLogger *logger = FairLogger::GetLogger();
+  logger->SetLogFileName("MyLog.log");    // define log file name
   // log to screen and to file 
   logger->SetLogToScreen(kTRUE);
   logger->SetLogToFile(kTRUE);
-  // Print very accurate output. Levels are LOW, MEDIUM and HIGH
-  logger->SetLogVerbosityLevel("HIGH");
- 
-  // -----   Create simulation run   ----------------------------------------
-  FairRunSim* run = new FairRunSim();
-  run->SetName("TGeant4");              // Transport engine
-  run->SetOutputFile(outFile);          // Output file
-  FairRuntimeDb* rtdb = run->GetRuntimeDb();
-  // ------------------------------------------------------------------------
+  logger->SetLogVerbosityLevel("HIGH");    // Print very accurate output. Levels are LOW, MEDIUM and HIGH
 
-  run->SetWriteRunInfoFile(kFALSE);  
-  // -----   Create media   -------------------------------------------------
-//  run->SetMaterials("media.geo");       // Materials
-  run->SetMaterials("media_pnd.geo");       // Materials
-  // ------------------------------------------------------------------------
+  TString outFile = outDir + "/test.mc_youngstest.root"; 
+  TString parFile = outDir + "/params_youngstest.root"; 
+
+    // -----   Create simulation run   ----------------------------------------
+    FairRunSim* run = new FairRunSim();
+    run->SetName("TGeant3");              // Transport engine
+    run->SetOutputFile(outFile);          // Output file
+    FairRuntimeDb* rtdb = run->GetRuntimeDb();
+    // ------------------------------------------------------------------------
+    
+    run->SetWriteRunInfoFile(kFALSE);  
+    // -----   Create media   -------------------------------------------------
+    //  run->SetMaterials("media.geo");       // Materials
+    run->SetMaterials("media_pnd.geo");       // Materials
+    // ------------------------------------------------------------------------
+    
+    // -----   Create geometry   ----------------------------------------------
+    
+    FairModule* cave= new FairCave("CAVE");
+    cave->SetGeometryFileName("cave_vacuum.geo"); 
+    run->AddModule(cave);
+    
+    FairModule* target= new FairTarget("SnTarget");
+    target->SetGeometryFileName("target.geo"); 
+    run->AddModule(target);
+    
+    FairDetector* spirit = new SPiRIT("SPiRITDetector", kTRUE);
+    //spirit->SetGeometryFileName("spirit.geo"); 
+    //spirit->SetGeometryFileName("tpc_prototype_ArCo2.root"); 
+    spirit->SetGeometryFileName("testingsave.root");
+    run->AddModule(spirit);
+    
+    // ------------------------------------------------------------------------
+    
+    // -----   Create and set magnetic field   --------------------------------
+    // Constant field
+    FairConstField *fMagField = new FairConstField();
+    fMagField -> SetField(0., 5., 0.); // in kG
+    // SetFieldRegion(xmin, xmax, ymin, ymax, zmin, zmax)
+    fMagField -> SetFieldRegion(-50, 50, -50, 50, -50, 400);
+    run -> SetField(fMagField);
+    // ------------------------------------------------------------------------
+    
+    // -----   Create PrimaryGenerator   --------------------------------------
+  cout << "testhere3" << endl;
+  FairPrimaryGenerator *primGen = new FairPrimaryGenerator();
+  run->SetGenerator(primGen);  
+  Int_t dnum=2;
+  const int gennum=pid[dnum].size();
+  cout << gennum << endl;
+  FairParticleGenerator *fIongen[gennum];
   
-  // -----   Create geometry   ----------------------------------------------
+  for (Int_t i=0; i<gennum; i++) {
+    fIongen[i] = new FairParticleGenerator(pid[dnum][i],1,pxl[dnum][i],pyl[dnum][i],pzl[dnum][i],0.0,-21.33,-3.52);
+    cout << pid[dnum][i] << "\t" << pxl[dnum][i] << "\t" << pyl[dnum][i] << "\t" << pzl[dnum][i] << "\t" << kinl[dnum][i] << endl;
+    primGen->AddGenerator(fIongen[i]);
+  }
+  cout << "testhere4" << endl;
 
-  FairModule* cave= new FairCave("CAVE");
-  cave->SetGeometryFileName("cave_vacuum.geo"); 
-  run->AddModule(cave);
 
-  FairModule* target= new FairTarget("SnTarget");
-  target->SetGeometryFileName("target.geo"); 
-  run->AddModule(target);
+    //primGen->AddTrack(pdg,pionpC[0][i],pionpC[1][i],pionpC[2][i],pionrC[0][i]*1.0E-13,pionrC[1][i]*1.0E-13-21.33,pionrC[2][i]*1.0E-13-3.52);
+    //fIongen[i] = new FairParticleGenerator(pdg,1,pionpC[0][i],pionpC[1][i],pionpC[2][i],pionrC[0][i]*1.0E-13,pionrC[1][i]*1.0E-13-21.33,pionrC[2][i]*1.0E-13-3.52);
+  //fIongen[i] = new FairParticleGenerator(pdg,1,px,py,pz,0.0,-21.33,-3.52);
+  //primGen->AddGenerator(fIongen[i]);
+    //primGen->AddTrack(pdg,pionpC[0][i],pionpC[1][i],pionpC[2][i],pionrC[0][i]*1.0E-13,pionrC[1][i]*1.0E-13-21.33,pionrC[2][i]*1.0E-13-3.52);
+    //}
+  //}
 
-  FairDetector* spirit = new SPiRIT("SPiRITDetector", kTRUE);
-  spirit->SetGeometryFileName("spirit.geo"); 
-//  spirit->SetGeometryFileName("tpc_prototype_ArCo2.root"); 
-  run->AddModule(spirit);
+  // if (partt==0) FairBoxGenerator* boxGen1 = new FairBoxGenerator(2212, 1);
+  // if (partt==1) FairBoxGenerator* boxGen1 = new FairBoxGenerator(1000010020, 1);
+  // if (partt==2) FairBoxGenerator* boxGen1 = new FairBoxGenerator(1000010030, 1);
+  // if (partt==3) FairBoxGenerator* boxGen1 = new FairBoxGenerator(1000020030, 1);
+  // if (partt==4) FairBoxGenerator* boxGen1 = new FairBoxGenerator(1000020040, 1);
+  // if (partt==5) FairBoxGenerator* boxGen1 = new FairBoxGenerator(211, 1);
+  // if (partt==6) FairBoxGenerator* boxGen1 = new FairBoxGenerator(-211, 1);
+  // if (partt==7) FairBoxGenerator* boxGen1 = new FairBoxGenerator(11, 1);
+  // if (partt==8) FairBoxGenerator* boxGen1 = new FairBoxGenerator(-11, 1);
+  // //FairBoxGenerator* boxGen1 = new FairBoxGenerator(1000020040, 1);
+  // boxGen1->SetPRange(0.25,0.5); //GeV/c ??
+  // boxGen1->SetPhiRange(0.,360.); //degrees
+  // boxGen1->SetThetaRange(0.,90.); //degrees
+  // boxGen1->SetXYZ(0.,-21.33, -3.52); // cm 
+  // primGen->AddGenerator(boxGen1);
+    
   // ------------------------------------------------------------------------
-
-  // -----   Create and set magnetic field   --------------------------------
-  // Constant field
-  FairConstField *fMagField = new FairConstField();
-  fMagField -> SetField(0., 50., 0.); // in kG
-  // SetFieldRegion(xmin, xmax, ymin, ymax, zmin, zmax)
-  fMagField -> SetFieldRegion(-50, 50, -50, 50, -20, 200);
-  run -> SetField(fMagField);
-  // ------------------------------------------------------------------------
-
-  // -----   Create PrimaryGenerator   --------------------------------------
-  FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
-  run->SetGenerator(primGen);
-
-  // Ion Generator
-  // FairIonGenerator(z, a, q[e], mult, px/A, py/A, pz/A[GeV], x, y, z[cm])                                             
-//  FairIonGenerator *fIongen= new FairIonGenerator(1, 1, 1, 1, 0., 0., 0.5, 0., 0., -12.231);
-  FairParticleGenerator *fIongen = new FairParticleGenerator(2212, 1, 0., 0., 0.15, 0., 0., -12.231);
-  primGen->AddGenerator(fIongen);
-
-  FairBoxGenerator* boxGen1 = new FairBoxGenerator(2212, 1);
-  boxGen1->SetPRange(.005,.005);
-  boxGen1->SetPhiRange(0.,0.);
-  boxGen1->SetThetaRange(0.,0.);
-  boxGen1->SetXYZ(0.,0.,-3.);
-  primGen->AddGenerator(boxGen1);
-
-
-  // ------------------------------------------------------------------------
-
+    
   run->SetStoreTraj(kTRUE);
-
+    
   // -----   Run initialisation   -------------------------------------------
+  //if (i==0) run->Init();
   run->Init();
   // ------------------------------------------------------------------------
-
+    
   // Set cuts for storing the trajectories.
   // Switch this on only if trajectories are stored.
   // Choose this cuts according to your needs, but be aware
   // that the file size of the output file depends on these cuts
-
-  FairTrajFilter* trajFilter = FairTrajFilter::Instance();
-  trajFilter->SetStepSizeCut(0.01); // 1 cm
-  // trajFilter->SetVertexCut(-2000., -2000., 4., 2000., 2000., 100.);
-  // trajFilter->SetMomentumCutP(10e-3); // p_lab > 10 MeV
-  // trajFilter->SetEnergyCut(0., 1.02); // 0 < Etot < 1.04 GeV
-  trajFilter->SetStorePrimaries(kTRUE);
-  trajFilter->SetStoreSecondaries(kTRUE);
+    
+  //FairTrajFilter* trajFilter = FairTrajFilter::Instance();
+  //trajFilter->SetStepSizeCut(0.01); // 1 cm
+  //trajFilter->SetStorePrimaries(kTRUE);
+  //trajFilter->SetStoreSecondaries(kTRUE);
   // ------------------------------------------------------------------------
-
+    
   // -----   Runtime database   ---------------------------------------------
-
+  //if (i==0) {
   Bool_t kParameterMerged = kTRUE;
   FairParRootFileIo* parOut = new FairParRootFileIo(kParameterMerged);
   parOut->open(parFile.Data());
   rtdb->setOutput(parOut);
+  //}
   rtdb->saveOutput();
   rtdb->print();
+  
   // ------------------------------------------------------------------------
-   
+    
   // -----   Start run   ----------------------------------------------------
   run->Run(nEvents);
+  //}
+
   // ------------------------------------------------------------------------
-//  run->CreateGeometryFile("data/geofile_full.root");
+  //  run->CreateGeometryFile("data/geofile_full.root");
   
   // -----   Finish   -------------------------------------------------------
   timer.Stop();
@@ -141,6 +282,10 @@ void run_mc(Int_t nEvents = 10)
   cout << "Real time " << rtime << " s, CPU time " << ctime 
        << "s" << endl << endl;
   // ------------------------------------------------------------------------
+
+  cout << mBeta << endl;
+
 }
 
 
+//}
