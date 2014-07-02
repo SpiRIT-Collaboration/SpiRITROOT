eaeace5ac759f7c83a906aec19cf323fe3b04d28
Genie Jhang <geniejhang@majimak.com>
2014-07-02 17:37:40 +0900

    Doxygen comments for STDecoderTask

--- SPiRIT/reco/STDecoderTask.hh
+++ SPiRIT/reco/STDecoderTask.hh
@@ -27,32 +27,47 @@
 // ROOT classes
 #include "TClonesArray.h"
 
+/**
+  * This class loads RAW data file from GET electronics and
+  * decodes binary data to human readable format. After that
+  * according to the given mapping and pedestal data, this arranges
+  * data from each channel to the SpiRIT-TPC padplane.
+ **/
 class STDecoderTask : public FairTask {
   public:
+    //! Constructor
     STDecoderTask();
+    //! Destructor
     ~STDecoderTask();
 
+    //! Setting the number of time buckets used when taking data
     void SetNumTbs(Int_t numTbs);
+    //! Setting raw data file
     void SetGraw(Char_t *filename);
+    //! Setting pedestal data file. If not set, internal pedestal calculation method will be used.
     void SetPedestal(Char_t *filename);
 
+    //! If set, decoded raw data is written in ROOT file with STRawEvent class.
     void SetPersistence(Bool_t value = kTRUE);
 
+    //! Initializing the task. This will be called when Init() method invoked from FairRun.
     virtual InitStatus Init();
+    //! Setting parameter containers. This will be called inbetween Init() and Run().
     virtual void SetParContainers();
+    //! Running the task. This will be called when Run() method invoked from FairRun.
     virtual void Exec(Option_t *opt);
 
   private:
-    STCore *fDecoder;
+    STCore *fDecoder;             //!< STConverter pointer
 
-    Char_t *fGrawFile;
-    Char_t *fPedestalFile;
-    Int_t fNumTbs;
+    Char_t *fGrawFile;            //!< Raw data file name
+    Char_t *fPedestalFile;        //!< Pedestal data file name
+    Int_t fNumTbs;                //!< The number of time buckets
 
-    Bool_t fIsPersistence;
+    Bool_t fIsPersistence;        //!< Persistence check variable
 
-    STDigiPar *fPar;
-    TClonesArray *fRawEventArray;
+    STDigiPar *fPar;              //!< Parameter read-out class pointer
+    TClonesArray *fRawEventArray; //!< STRawEvent container
 
   ClassDef(STDecoderTask, 1);
 };
