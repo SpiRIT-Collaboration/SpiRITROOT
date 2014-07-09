a49b9a91928959a760685d79c010b73e93864527
Genie Jhang <geniejhang@majimak.com>
2014-07-09 18:34:58 +0900

    - TpcRiemannHit is copied from FOPIROOT. - It is changed into STRiemannHit and syntax is manipulated. - I think it will be working as soon as STCluster class is completed.

---
+++ SPiRIT/reco/RiemannTracking/STRiemannHit.cc
@@ -0,0 +1,105 @@
+//-----------------------------------------------------------------
+// Description:
+//      Conformal Mapping of a cluster
+//      (x,y) -> (r,phi)-> riemann sphere
+//
+// Environment:
+//      Software developed for the SpiRIT-TPC at RIBF-RIKEN
+//
+// Original Author:
+//      Sebastian Neubert    TUM
+//
+// Author List:
+//      Genie Jhang          Korea University
+//-----------------------------------------------------------------
+
+// SpiRITROOT classes
+#include "STRiemannHit.hh"
+#include "STRiemannTrack.hh"
+
+// STL
+#include <cmath>
+
+// ROOT classes
+#include "TMath.h"
+
+ClassImp(STRiemannHit)
+
+STRiemannHit::STRiemannHit()
+{
+  InitVariables(0, 0);
+}
+
+STRiemannHit::STRiemannHit(Double_t riemannScale)
+{
+  InitVariables(0, 0, riemannScale);
+}
+
+STRiemannHit::STRiemannHit(Double_t r, Double_t phi, Double_t riemannScale)
+{
+  fCluster = NULL;
+  fRiemannScale = riemannScale;
+
+  InitVariables(r, phi, riemannScale);
+}
+
+STRiemannHit::STRiemannHit(STCluster* cluster, Double_t riemannScale)
+{
+  fCluster = cluster;
+  fRiemannScale = riemannScale;
+
+/*
+  TVector2 proj(cluster -> pos().X(), cluster -> pos().Y());
+  Double_t r = proj.Mod()/fRiemannScale;
+  Double_t phi = proj.Phi();
+  
+
+  InitVariables(r, phi, riemannScale);
+  */
+  InitVariables(0, 0, riemannScale);
+}
+
+
+STRiemannHit::~STRiemannHit()
+{
+}
+
+void STRiemannHit::InitVariables(Double_t r, Double_t phi, Double_t riemannScale)
+{
+  fS = 0;
+  fAngleOnHelix = 0;
+  fRiemannScale = riemannScale;
+
+  Double_t r2 = r*r;
+  Double_t d = 1 + r2;
+
+  fX.SetX(r*cos(phi)/d);
+  fX.SetY(r*sin(phi)/d);
+  fX.SetZ(r2/d);
+
+  fLogger = FairLogger::GetLogger();
+}
+
+// Simple getter and setter methods -----------------------------------------------------------
+const TVector3 &STRiemannHit::GetX()             const        { return fX; }
+     STCluster *STRiemannHit::GetCluster()       const        { return fCluster; }
+      Double_t  STRiemannHit::GetS()             const        { return fS; }
+      Double_t  STRiemannHit::GetAngleOnHelix()  const        { return fAngleOnHelix; }
+      Double_t  STRiemannHit::GetRiemannScale()  const        { return fRiemannScale; }
+
+      Double_t  STRiemannHit::SetS(Double_t value)            { fS = value; }
+      Double_t  STRiemannHit::SetAngleOnHelix(Double_t value) { fAngleOnHelix = value; }
+// --------------------------------------------------------------------------------------------
+
+Double_t
+STRiemannHit::GetZ() const
+{
+  if(fCluster == NULL){
+    fLogger -> Warning(MESSAGE_ORIGIN, "No cluster available!");
+
+    return 0;
+  }
+
+  return 0; //fCluster -> pos().Z(); <<-- to be modified
+}
+
