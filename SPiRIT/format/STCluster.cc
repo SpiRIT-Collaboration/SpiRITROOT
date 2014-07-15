385cc6c2d42902e304ad291a6764a9749c10644b
Genie Jhang <geniejhang@majimak.com>
2014-07-15 17:07:08 +0900

    Fundamental structure of STCluster is added.

---
+++ SPiRIT/format/STCluster.cc
@@ -0,0 +1,43 @@
+// =================================================
+//  STCluster Class                          
+//                                                  
+//  Description:                                    
+//    Container for a cluster data
+//                                                  
+//  Genie Jhang ( geniejhang@majimak.com )          
+//  2014. 07. 15                                    
+// =================================================
+
+#include "STCluster.hh"
+
+ClassImp(STCluster)
+
+STCluster::STCluster()
+{
+  fPosition = TVector3(0, 0, -1000);
+  fPosError = TVector3(0., 0., 0.);
+
+  for (Int_t iElem = 0; iElem < 9; iElem++)
+    fCovariant(iElem/3, iElem%3) = 0;
+}
+
+STCluster::STCluster(STCluster *cluster)
+{
+  fPosition = cluster -> GetPosition();
+  fPosError = cluster -> GetPosError();
+  fCovariant = cluster -> GetCovMatrix();
+
+  fHitNoArray = *(cluster -> GetHitNumbers());
+}
+
+TVector3 STCluster::GetPosition()  { return fPosition; }
+TVector3 STCluster::GetPosError()  { return fPosError; }
+TMatrixD STCluster::GetCovMatrix() { return fCovariant; }
+
+Int_t STCluster::GetNumHits()                  { return fHitNoArray.size(); }
+std::vector<Int_t> *STCluster::GetHitNumbers() { return &fHitNoArray; }
+
+void STCluster::AddHit(STHit *hit)
+{
+  // Calculating cluster position, error and covariant matrix
+}
---
+++ SPiRIT/format/STCluster.hh
@@ -0,0 +1,47 @@
+// =================================================
+//  STCluster Class                          
+//                                                  
+//  Description:                                    
+//    Container for a cluster data
+//                                                  
+//  Genie Jhang ( geniejhang@majimak.com )          
+//  2014. 07. 15                                    
+// =================================================
+
+#ifndef STCLUSTER_H
+#define STCLUSTER_H
+
+#include "STHit.hh"
+
+#include "TObject.h"
+#include "TVector3.h"
+#include "TMatrixD.h"
+
+#include <vector>
+
+class STCluster : public TObject 
+{
+  public:
+    STCluster();
+    STCluster(STCluster *cluster);
+
+    TVector3 GetPosition();
+    TVector3 GetPosError();
+    TMatrixD GetCovMatrix();
+
+    Int_t GetNumHits();
+    std::vector<Int_t> *GetHitNumbers();
+
+    void AddHit(STHit *hit);
+
+  private:
+    TVector3 fPosition;
+    TVector3 fPosError;
+    TMatrixD fCovariant(3, 3);
+
+    std::vector<Int_t> fHitNoArray;
+
+  ClassDef(STCluster, 1)
+}
+
+#endif
