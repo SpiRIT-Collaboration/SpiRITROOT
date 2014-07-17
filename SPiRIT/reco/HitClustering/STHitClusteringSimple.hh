fd1279f8f075304e95d03cb5103e6f801ad6325b
Genie Jhang <geniejhang@majimak.com>
2014-07-17 11:46:08 +0900

    Basic structure of STHitClusteringSimple is added.

---
+++ SPiRIT/reco/HitClustering/STHitClusteringSimple.hh
@@ -0,0 +1,38 @@
+//-----------------------------------------------------------
+// Description:
+//   Simple version of clustering hits
+//
+// Environment:
+//   Software developed for the SPiRIT-TPC at RIKEN
+//
+// Author List:
+//   Genie Jhang     Korea University     (original author)
+//-----------------------------------------------------------
+
+#ifndef STHITCLUSTERINGSIMPLE_H
+#define STHITCLUSTERINGSIMPLE_H
+
+// SpiRITROOT clasees
+#include "STEvent.hh"
+
+// FairROOT classes
+#include "FairLogger.h"
+
+// ROOT classes
+#include "Rtypes.h"
+
+class STHitClusteringSimple
+{
+  public:
+    STHitClusteringSimple();
+    ~STHitClusteringSimple();
+
+    Bool_t FindCluster(STEvent *eventHArray, STEvent *eventHCArray);
+
+  private:
+    FairLogger *fLogger;
+
+  ClassDef(STHitClusteringSimple, 1)
+};
+
+#endif
