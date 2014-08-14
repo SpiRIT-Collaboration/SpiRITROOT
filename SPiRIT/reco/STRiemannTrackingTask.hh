49cf7a258fce26196bb40d39c7172159971d5965
Genie Jhang <geniejhang@majimak.com>
2014-08-14 16:35:00 +0900

    Working STRiemannTrackingTask. It's working but not properly working. We need fine tuning. Related issues #19, #20.

--- SPiRIT/reco/STRiemannTrackingTask.hh
+++ SPiRIT/reco/STRiemannTrackingTask.hh
@@ -21,8 +21,8 @@
 #include "FairLogger.h"
 
 // SpiRITROOT classes 
-//#include "STRiemannTrack.hh"
-//#include "STCluster.hh"
+#include "STRiemannTrack.hh"
+#include "STHitCluster.hh"
 
 class TClonesArray;
 class STRiemannTrackFinder;
@@ -34,40 +34,40 @@ class STRiemannTrackingTask : public FairTask
     STRiemannTrackingTask();
     ~STRiemannTrackingTask();
 
-    void SetVerbose(Bool_t value = kTRUE);     //!< If set, much logging information will be written in log file.
-    void SetPersistence(Bool_t value = kTRUE); //!< store processed data into output ROOT file
+    void SetVerbose(Bool_t value = kTRUE);     /// If set, much logging information will be written in log file.
+    void SetPersistence(Bool_t value = kTRUE); /// store processed data into output ROOT file
 
-    void SetSortingParameters(Bool_t sortingMode = kTRUE,  //!< false: sort only according to _sorting; true: use internal sorting when adding hits to trackcands
-                               Int_t sorting = 1,          //!< -1: no sorting, 0: sort Clusters by X, 1: Y, 2: Z, 3: R, 4: distance to interaction point, 5: Phi, -5: -Phi
-                            Double_t interactionZ = 0);    //!< set if you use sorting = 4
+    void SetSortingParameters(Bool_t sortingMode = kTRUE,  /// false: sort only according to _sorting; true: use internal sorting when adding hits to trackcands
+                               Int_t sorting = 1,          /// -1: no sorting, 0: sort Clusters by X, 1: Y, 2: Z, 3: R, 4: distance to interaction point, 5: Phi, -5: -Phi
+                            Double_t interactionZ = 0);    /// set if you use sorting = 4
 
-    void SetMultistepParameters(Bool_t doMultistep,      //!< do a multistep approach:
-                                                         //!<  1. find steep tracks (presort clusters along z)
-                                                         //!<  2. find circle tracks  (presort clusters by angle)
-                                                         //!<  3. find all other tracks (presort clusters by decreasing radius)
-                                UInt_t minHitsZ = 10,    //!< minimum number of hits for a track to be found in step 1
-                                UInt_t minHitsR = 10,    //!< minimum number of hits for a track to be found in step 1
-                                UInt_t minHitsPhi = 10); //!< minimum number of hits for a track to be found in step 2
+    void SetMultistepParameters(Bool_t doMultistep,      /// do a multistep approach:
+                                                         ///  1. find steep tracks (presort clusters along z)
+                                                         ///  2. find circle tracks  (presort clusters by angle)
+                                                         ///  3. find all other tracks (presort clusters by decreasing radius)
+                                UInt_t minHitsZ = 10,    /// minimum number of hits for a track to be found in step 1
+                                UInt_t minHitsR = 10,    /// minimum number of hits for a track to be found in step 1
+                                UInt_t minHitsPhi = 10); /// minimum number of hits for a track to be found in step 2
 
-    void SetTrkFinderParameters(Double_t proxcut,          //!< proximity cut in 3D
-                                Double_t helixcut,         //!< distance to helix cut
-                                  UInt_t minpointsforfit,  //!< minimum number of hits in track before a helix is fitted
-                                Double_t zStretch = 1.);   //!< stretch proximity cut in z direction
+    void SetTrkFinderParameters(Double_t proxcut,          /// proximity cut in 3D
+                                Double_t helixcut,         /// distance to helix cut
+                                  UInt_t minpointsforfit,  /// minimum number of hits in track before a helix is fitted
+                                Double_t zStretch = 1.);   /// stretch proximity cut in z direction
 
-    void SetMaxRMS(Double_t value);   //!< max RMS of distances to helix for a track to be written out
+    void SetMaxRMS(Double_t value);   /// max RMS of distances to helix for a track to be written out
 
-    void SetMergeTracks(Bool_t mergeTracks = kTRUE);   //!< merge tracklets
+    void SetMergeTracks(Bool_t mergeTracks = kTRUE);   /// merge tracklets
 
     void SetMergeCurlers(Bool_t mergeCurlers = kTRUE, Double_t blowUp = 5.);  //!< merge curlers
 
-    void SetTrkMergerParameters(Double_t TTproxcut,    //!< proximity cut in 3D
-                                Double_t TTdipcut,     //!< cut on difference of dip angles of tracklets
-                                Double_t TThelixcut,   //!< distance of the two helices
-                                Double_t TTplanecut);  //!< cut on rms of distances of the riemann hits to intersection of the plane with the sphere of a combined fit
+    void SetTrkMergerParameters(Double_t TTproxcut,    /// proximity cut in 3D
+                                Double_t TTdipcut,     /// cut on difference of dip angles of tracklets
+                                Double_t TThelixcut,   /// distance of the two helices
+                                Double_t TTplanecut);  /// cut on rms of distances of the riemann hits to intersection of the plane with the sphere of a combined fit
 
-    void SetRiemannScale(Double_t riemannscale = 8.7);  //!< blow up factor of the riemann sphere
+    void SetRiemannScale(Double_t riemannscale = 8.7);  /// blow up factor of the riemann sphere
     
-    void SkipCrossingAreas(Bool_t value = kTRUE)    //!< skip and remove hits which would match to more than one track at highest correlator level
+    void SkipCrossingAreas(Bool_t value = kTRUE);    /// skip and remove hits which would match to more than one track at highest correlator level
 
     virtual InitStatus Init();
     virtual void SetParContainers();
@@ -77,8 +77,10 @@ class STRiemannTrackingTask : public FairTask
   private:
     FairLogger *fLogger;
 
-    void buildTracks(STRiemannTrackFinder *trackfinder,
-                     std::vector<TpcCluster *> *clusterBuffer,
+    Bool_t fVerbose;
+
+    void BuildTracks(STRiemannTrackFinder *trackfinder,
+                     std::vector<STHitCluster *> *clusterBuffer,
                      std::vector<STRiemannTrack*> *TrackletList,
                      Int_t sorting,
                      UInt_t minHits,
@@ -86,15 +88,15 @@ class STRiemannTrackingTask : public FairTask
                      Bool_t skipCrossingAreas = kTRUE,
                      Bool_t skipAndDelete = kTRUE);
 
-    TClonesArray *fClusterArray;
+    TClonesArray *fEventHCArray;
+    TClonesArray *fEventHCMArray;
     TClonesArray *fMvdArray;
     TClonesArray *fRiemannTrackArray;
     TClonesArray *fRiemannHitArray;
 
     STDigiPar *fPar;
 
-    std::map<UInt_t, std::vector<TpcCluster *> *> fBufferMap;
-    std::vector<TpcCluster *> *fClusterBuffer;
+    std::vector<STHitCluster *> *fClusterBuffer;
     std::vector<STRiemannTrack *> fRiemannList;
 
     STRiemannTrackFinder *fTrackFinder;
@@ -102,10 +104,7 @@ class STRiemannTrackingTask : public FairTask
 
     Bool_t fIsPersistence;
 
-    UInt_t fNSectors;
-    Double_t fMaxRadius; //!< outer radius of padplane
-    Double_t fTpcOffset;
-    Double_t fTpcLength;
+    Double_t fMaxRadius; /// outer radius of padplane
 
     Int_t fCounter;
 
@@ -140,7 +139,10 @@ class STRiemannTrackingTask : public FairTask
 
     Double_t fMaxRMS;
 
-  ClassDef(STRiemannTrackingTask, 1)
+    TString fRiemannTrackBranchName;
+    TString fRiemannHitBranchName;
+
+  ClassDef(STRiemannTrackingTask, 1);
 };
 
 #endif
