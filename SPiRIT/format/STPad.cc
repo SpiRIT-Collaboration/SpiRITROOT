1b6db342ad0f9332f76fc4784cee00da8e3dc8de
Genie Jhang <geniejhang@majimak.com>
2014-08-25 22:37:42 +0900

    Flag for checking gain calibration added.

--- SPiRIT/format/STPad.cc
+++ SPiRIT/format/STPad.cc
@@ -44,76 +44,24 @@ void STPad::Initialize()
 }
 
 // setters
-void STPad::SetLayer(Int_t val)
-{
-  fLayer = val;
-}
-
-void STPad::SetRow(Int_t val)
-{
-  fRow = val;
-}
-
-void STPad::SetRawADC(Int_t *val)
-{
-  memcpy(val, fRawAdc, sizeof(fRawAdc));
-}
-
-void STPad::SetRawADC(Int_t idx, Int_t val)
-{
-  fRawAdc[idx] = val;
-}
-
-void STPad::SetPedestalSubtracted(Bool_t val)
-{
-  fIsPedestalSubtracted = val;
-}
-
-void STPad::SetMaxADCIdx(Int_t val)
-{
-  fMaxAdcIdx = val;
-}
-
-void STPad::SetADC(Double_t *val)
-{
-  memcpy(fAdc, val, sizeof(fAdc));
-}
-
-void STPad::SetADC(Int_t idx, Double_t val)
-{
-  fAdc[idx] = val;
-}
+void STPad::SetLayer(Int_t val)               { fLayer = val; }
+void STPad::SetRow(Int_t val)                 { fRow = val; }
+void STPad::SetRawADC(Int_t *val)             { memcpy(val, fRawAdc, sizeof(fRawAdc)); }
+void STPad::SetRawADC(Int_t idx, Int_t val)   { fRawAdc[idx] = val; }
+void STPad::SetPedestalSubtracted(Bool_t val) { fIsPedestalSubtracted = val; }
+void STPad::SetGainCalibrated(Bool_t val)     { fIsGainCalibrated = val; }
+void STPad::SetMaxADCIdx(Int_t val)           { fMaxAdcIdx = val; }
+void STPad::SetADC(Double_t *val)             { memcpy(fAdc, val, sizeof(fAdc)); }
+void STPad::SetADC(Int_t idx, Double_t val)   { fAdc[idx] = val; }
 
 // getters
-Int_t STPad::GetLayer()
-{
-  return fLayer;
-}
-
-Int_t STPad::GetRow()
-{
-  return fRow;
-}
-
-Int_t *STPad::GetRawADC()
-{
-  return fRawAdc;
-}
-
-Int_t STPad::GetRawADC(Int_t idx)
-{
-  return fRawAdc[idx];
-}
-
-Int_t STPad::GetMaxADCIdx()
-{
-  return fMaxAdcIdx;
-}
-
-Bool_t STPad::IsPedestalSubtracted()
-{
-  return fIsPedestalSubtracted;
-}
+ Int_t  STPad::GetLayer()             { return fLayer; }
+ Int_t  STPad::GetRow()               { return fRow; }
+ Int_t *STPad::GetRawADC()            { return fRawAdc; }
+ Int_t  STPad::GetRawADC(Int_t idx)   { return fRawAdc[idx]; }
+ Int_t  STPad::GetMaxADCIdx()         { return fMaxAdcIdx; }
+Bool_t  STPad::IsPedestalSubtracted() { return fIsPedestalSubtracted; }
+Bool_t  STPad::IsGainCalibrated()     { return fIsGainCalibrated; }
 
 Double_t *STPad::GetADC()
 {
