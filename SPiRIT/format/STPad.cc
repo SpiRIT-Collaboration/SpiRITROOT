9dc84e40f6addf1dcd138c626b0adfe337e3a2d5
Genie Jhang <geniejhang@majimak.com>
2014-08-26 15:04:42 +0900

    Revert "A method for delaying signal back and forth is added." This reverts commit 69a329a276be36f503b6dece7f64d3b49a7515a6.
    I think it would be better to add another class for delaying job
    and STCore will call that class to delay the signal so that
    STPad class is left as just a container class.

--- SPiRIT/format/STPad.cc
+++ SPiRIT/format/STPad.cc
@@ -33,17 +33,14 @@ STPad::~STPad()
 
 void STPad::Initialize()
 {
-  fIsPedestalSubtracted = kFALSE;
-  fIsGainCalibrated = kFALSE;
+ fIsPedestalSubtracted = 0;
+ fMaxAdcIdx = 0;
 
-  fMaxAdcIdx = 0;
-  fDelay = 0;
+ memset(fRawAdc, 0, sizeof(fRawAdc));
+ memset(fAdc, 0, sizeof(fAdc));
 
-  memset(fRawAdc, 0, sizeof(fRawAdc));
-  memset(fAdc, 0, sizeof(fAdc));
-
-  fRow = -1;
-  fLayer = -1;
+ fRow = -1;
+ fLayer = -1;
 }
 
 // setters
@@ -53,7 +50,6 @@ void STPad::SetRawADC(Int_t *val)             { memcpy(val, fRawAdc, sizeof(fRaw
 void STPad::SetRawADC(Int_t idx, Int_t val)   { fRawAdc[idx] = val; }
 void STPad::SetPedestalSubtracted(Bool_t val) { fIsPedestalSubtracted = val; }
 void STPad::SetGainCalibrated(Bool_t val)     { fIsGainCalibrated = val; }
-void STPad::SetDelay(Int_t val)               { fDelay = val; }
 void STPad::SetMaxADCIdx(Int_t val)           { fMaxAdcIdx = val; }
 void STPad::SetADC(Double_t *val)             { memcpy(fAdc, val, sizeof(fAdc)); }
 void STPad::SetADC(Int_t idx, Double_t val)   { fAdc[idx] = val; }
@@ -64,7 +60,6 @@ void STPad::SetADC(Int_t idx, Double_t val)   { fAdc[idx] = val; }
  Int_t *STPad::GetRawADC()            { return fRawAdc; }
  Int_t  STPad::GetRawADC(Int_t idx)   { return fRawAdc[idx]; }
  Int_t  STPad::GetMaxADCIdx()         { return fMaxAdcIdx; }
- Int_t  STPad::GetDelay()             { return fDelay; }
 Bool_t  STPad::IsPedestalSubtracted() { return fIsPedestalSubtracted; }
 Bool_t  STPad::IsGainCalibrated()     { return fIsGainCalibrated; }
 
@@ -76,17 +71,7 @@ Double_t *STPad::GetADC()
     return 0;
   }
 
-  Double_t *adc = new Double_t[512];
-  for (Int_t iTb = 0; iTb < 512; iTb++) {
-    if (iTb + fDelay < 0)
-      continue;
-    else if (iTb + fDelay > 511)
-      break;
-    
-    adc[iTb] = fAdc[iTb + fDelay];
-  }
-
-  return adc;
+  return fAdc;
 }
 
 Double_t STPad::GetADC(Int_t idx)
@@ -97,8 +82,6 @@ Double_t STPad::GetADC(Int_t idx)
     return -4;
   }
 
-  if (idx + fDelay < 0 || idx + fDelay > 511)
-    return 0;
-  else 
-    return fAdc[idx + fDelay];
+  return fAdc[idx];
 }
+
