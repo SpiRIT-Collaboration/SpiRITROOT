d8cd33ca0bc867b01b1433b12f17ad18370eddc3
Genie Jhang <geniejhang@majimak.com>
2014-09-08 18:36:06 +0900

    STGenerator: bug fix when fNumEvents is negative.

--- SPiRIT/reco/STConverter/STGenerator.cc
+++ SPiRIT/reco/STConverter/STGenerator.cc
@@ -223,9 +223,9 @@ STGenerator::SelectEvents(Int_t numEvents, Int_t *eventList)
   fNumEvents = numEvents;
 
   if (fNumEvents < 0)
-    cout << "== [STGenerator] Events in passed event list will be excluded!" << endl;
+    cout << "== [STGenerator] Events in passed event list will be excluded until event ID " << -fNumEvents << "!" << endl;
   else if (fNumEvents > 0)
-    cout << "== [STGenerator] Events in passed event are processed!" << endl;
+    cout << "== [STGenerator] Events in passed event list are processed!" << endl;
   else {
     cout << "== [STGenerator] All events are processed!" << endl;
 
@@ -298,6 +298,7 @@ STGenerator::GeneratePedestal()
 
   STRawEvent *event = NULL;
   Int_t eventID = 0;
+  Int_t numExcluded = 0;
   while ((event = fCore -> GetRawEvent())) {
     if (fNumEvents == 0) {}
     else if (fNumEvents > 0) {
@@ -306,10 +307,10 @@ STGenerator::GeneratePedestal()
       else if (fEventList[eventID] != event -> GetEventID())
         continue;
     } else if (fNumEvents < 0) {
-      if (eventID == -fNumEvents)
-        break;
-      else if (fEventList[eventID] == event -> GetEventID())
+      if (numExcluded < -fNumEvents && fEventList[numExcluded] == event -> GetEventID()) {
+        numExcluded++;
         continue;
+      }
     }
 
     cout << "[STGenerator] Start processing event: " << event -> GetEventID() << endl;
