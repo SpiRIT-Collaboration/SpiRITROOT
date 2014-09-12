849b0bc5c3eed178e5e90660dffc2f32e9998ecf
ejungwoo <phyjics@gmail.com>
2014-09-12 23:59:12 +0900

    Changes in CMakeList and LinkDef for digi directory compilation

--- SPiRIT/digi/STDigiLinkDef.h
+++ SPiRIT/digi/STDigiLinkDef.h
@@ -4,8 +4,11 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ class STPadResponse+;
+
 #pragma link C++ class STClusterizerTask+;
 #pragma link C++ class STDriftTask+;
 #pragma link C++ class STAvalancheTask+;
+#pragma link C++ class STPadResponseTask+;
 
 #endif
