a3d059cc1b661e2e33aa345d9b6e1a095e6eaed0
Genie Jhang <geniejhang@majimak.com>
2014-09-18 05:11:47 +0900

    makeGainCalibration.C:RMS inclusion factor example is added.

--- SPiRIT/user_macro/makeGainCalibration.C
+++ SPiRIT/user_macro/makeGainCalibration.C
@@ -3,7 +3,7 @@
 
   STGenerator *g = new STGenerator("gain");
   g -> SetParameterDir("parameters");
-  g -> SetPedestalData("PEDESTAL_DATA.root");
+  g -> SetPedestalData("PEDESTAL_DATA.root", 0);
   g -> SetPositivePolarity(); // comment out or add kFALSE for negative polarity
   g -> SetPersistence();      // comment out for disabling store raw data
   g -> SetOutputFile("OUTPUT_FILENAME.root");
