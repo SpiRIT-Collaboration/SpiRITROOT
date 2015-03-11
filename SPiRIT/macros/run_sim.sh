4f7f2fc913f0be56a33dae1ca8e4cf2161becb27
ejungwoo <phyjics@gmail.com>
2015-03-12 04:27:05 +0900

    run_sim.sh removed

--- SPiRIT/macros/run_sim.sh
+++ SPiRIT/macros/run_sim.sh
@@ -1,4 +1,6 @@
+#root -l -q 'run_mc_singleTrack.C("test0")';
 root -l -q 'run_mc.C("test0")';
 root -l -q 'run_digi.C("test0")';
 root -l -q 'run_reco_mc.C("test0")';
-root -l 'newEventDisplay_mc.C("test0")';
+root -l 'run_eve.C("test0")';
+#root -l 'newEventDisplay_mc.C("test0")';
