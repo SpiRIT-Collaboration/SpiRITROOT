614b814a4bfd6542e6bbcbe273f0546a204c134e
ejungwoo <phyjics@gmail.com>
2015-03-12 04:42:23 +0900

    proper edit to last commit

---
+++ SPiRIT/macros/run_sim.py
@@ -0,0 +1,96 @@
+import os
+import sys
+
+tag  = sys.argv[1]
+mode = sys.argv[2]
+
+
+
+
+
+modePossibleList = ['mc','digi','reco','eve','full','dummy']
+runFlag = False
+
+modeList = []
+if mode.find("+") != -1 : 
+  modeList = mode.split("+")
+  mode = mode.replace("+"," + ")
+if not modeList : modeList = [mode]
+
+for imode in modeList :
+  checkFlag = False
+  for modePossible in modePossibleList : 
+    if(imode == modePossible) : checkFlag = True
+  if checkFlag == False : 
+    runFlag = False
+    break
+  runFlag = True
+
+
+
+
+
+print
+print "======== SPiRIT Simulation Macro ========"
+print 
+print "Tag name :", tag
+print "Run mode :", mode
+print
+
+if not runFlag :
+  print "Run mode must be one of :"
+  for modePossible in modePossibleList : print " -" , modePossible 
+  print "or combination of these using \"+\" sign without black space." 
+  print "Combination should not jump over the order of simulation steps."
+  print 'Meaning "mc+digi+reco" is good, "mc+reco" is not.'
+  print 
+  print "Ending program."
+  sys.exit()
+
+
+
+
+dirSPiRIT = os.environ["SPIRITDIR"]
+dirGeom   = dirSPiRIT + "/geometry/"
+dirConf   = dirSPiRIT + "/gconfig/"
+os.environ["GEOMPATH"] = dirGeom
+os.environ["CONFIG_DIR"] = dirConf
+
+pathGeomSPiRIT      = dirGeom + "geomSPiRIT.root"
+pathGeomSPiRITMacro = dirGeom + "geomSPiRIT.C"
+existGeom = os.path.exists(pathGeomSPiRIT)
+print "Checking SPiRIT geometry file ..." , existGeom
+if not existGeom : 
+  print "  Creating geometry file ..."
+  print "  Running macro file :" , pathGeomSPiRITMacro
+  print
+  os.system("root -q -l " + pathGeomSPiRITMacro)
+  print
+
+existData = os.path.exists("data")
+print "Checking data directory ..." , existData
+if not existData : 
+  print "  Creating data directory ..."
+  print
+  os.system("mkdir data")
+  print
+
+
+
+
+
+print 
+for imode in modeList :
+  if imode == "full" :
+    os.system("root -l -q 'run_mc.C(\""+tag+"\")'")
+    os.system("root -l -q 'run_digi.C(\""+tag+"\"'")
+    os.system("root -l -q 'run_reco_mc.C(\""+tag+"\")'")
+    os.system("root -l    'run_eve.C(\""+tag+"\")'")
+  if imode == "mc"   : os.system("root -l -q 'run_mc.C(\""+tag+"\")'")
+  if imode == "digi" : os.system("root -l -q 'run_digi.C(\""+tag+"\")'")
+  if imode == "reco" : os.system("root -l -q 'run_reco_mc.C(\""+tag+"\")'")
+  if imode == "eve"  : os.system("root -l    'run_eve.C(\""+tag+"\")'")
+  if imode == "dummy" : print "dummy run."
+
+print
+print "Ending program."
--- SPiRIT/macros/run_sim.sh
+++
@@ -1,6 +0,0 @@
-#root -l -q 'run_mc_singleTrack.C("test0")';
-root -l -q 'run_mc.C("test0")';
-root -l -q 'run_digi.C("test0")';
-root -l -q 'run_reco_mc.C("test0")';
-root -l 'run_eve.C("test0")';
-#root -l 'newEventDisplay_mc.C("test0")';
