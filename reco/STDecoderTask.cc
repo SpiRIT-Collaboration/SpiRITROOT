38ec8dea9bc31dc60d603be4cae49724fc95bab1
Genie Jhang <geniejhang@majimak.com>
2015-05-18 15:46:06 +0900

    STDecoderTask: Decoding start number can be changed.

--- reco/STDecoderTask.cc
+++ reco/STDecoderTask.cc
@@ -160,7 +160,7 @@ STDecoderTask::Exec(Option_t *opt)
 {
   fRawEventArray -> Delete();
 
-  STRawEvent *rawEvent = fDecoder -> GetRawEvent();
+  STRawEvent *rawEvent = fDecoder -> GetRawEvent(FairRootManager::Instance() -> GetEntryNr());
 
   if (rawEvent != NULL)
     new ((*fRawEventArray)[0]) STRawEvent(rawEvent);
