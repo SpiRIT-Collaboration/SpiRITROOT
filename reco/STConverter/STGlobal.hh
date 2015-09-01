83adc01b0f2a668cccf682d196e7f3fdef3b94a9
Genie Jhang <geniejhang@majimak.com>
2015-09-01 12:26:44 +0900

    STGlobal: '#define' parameters are defined here.

---
+++ reco/STConverter/STGlobal.hh
@@ -0,0 +1,8 @@
+#ifndef STGLOBAL
+#define STGLOBAL
+
+// If set, gain calibration parameters are calculated in Pulser voltage vs ADC plane.
+// If not set, in ADC vs Pulser voltage.
+//#define VVSADC
+
+#endif
