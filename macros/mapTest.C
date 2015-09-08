ce11ed6cbf89d0b3c779a5e379dc7592eceb259a
Genie Jhang <geniejhang@majimak.com>
2015-09-08 17:29:02 +0900

    Removed unused macro

--- macros/mapTest.C
+++
@@ -1,8 +0,0 @@
-void mapTest() {
-  STMapTest *test = new STMapTest();
-  test -> SetAGETMap("../parameters/AGET.map");
-  test -> SetUAMap("../parameters/UnitAsAd.map");
-
-  test -> ShowAGETMap();
-  test -> ShowUAMap();
-}
