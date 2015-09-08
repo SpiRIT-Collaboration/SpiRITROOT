37443dfcc9eda6a37f749d81f01831f7a52d0318
ejungwoo <phyjics@gmail.com>
2015-09-08 15:23:56 +0900

    STDebugLogger: Added step histogram and 2D histogram.

--- util/STDebugLogger.cc
+++ util/STDebugLogger.cc
@@ -3,6 +3,15 @@
 ClassImp(STDebugLogger)
 
 STDebugLogger* STDebugLogger::fInstance = NULL;
+STDebugLogger* STDebugLogger::Create(TString name)
+{
+  if (fInstance == NULL)
+    fInstance = new STDebugLogger(name);
+  else 
+    std::cout << "STDebugLogger already exist!" << std::endl;
+
+  return fInstance;
+}
 
 STDebugLogger* STDebugLogger::Instance() 
 {
@@ -16,12 +25,24 @@ STDebugLogger::STDebugLogger()
 {
   if (fInstance != NULL) throw;
   fInstance = this;
-  fOutFile = new TFile("spirit_debug.root","RECREATE");
+
+  fOutFile = new TFile("st_debug_logger.root","RECREATE");
+  fMaxBranchIdx = 0;
+}
+
+STDebugLogger::STDebugLogger(TString name)
+{
+  if (fInstance != NULL) throw;
+  fInstance = this;
+
+  name = name + ".root";
+  fOutFile = new TFile(name,"RECREATE");
+  fMaxBranchIdx = 0;
 }
 
 STDebugLogger::~STDebugLogger()
 {
-  if(fOutFile != NULL)
+  if (fOutFile != NULL)
   {
     fOutFile -> Close();
     delete fOutFile;
@@ -32,35 +53,118 @@ void
 STDebugLogger::Write()
 {
   fOutFile -> cd();
-  std::map<TString, TH1D*>::iterator it = fMapHist.begin();
 
-  while (it != fMapHist.end()) {
-    std::cout << "STDebugLogger::Writing histogram " << it->first << std::endl;
-    it -> second -> Write();
-    delete it -> second;
-    ++it;
+  std::map<TString, TH1D*>::iterator it1 = fMapHist1.begin();
+  while (it1 != fMapHist1.end()) 
+  {
+    std::cout << "STDebugLogger::Writing 1D Histogram " << it1 -> first << std::endl;
+    it1 -> second -> Write();
+    delete it1 -> second;
+    it1++;
+  }
+
+  std::map<TString, TH2D*>::iterator it2 = fMapHist2.begin();
+  while (it2 != fMapHist2.end()) 
+  {
+    std::cout << "STDebugLogger::Writing 2D Histogram " << it2 -> first << std::endl;
+    it2 -> second -> Write();
+    delete it2 -> second;
+    it2++;
   }
 
   if (fOutFile != NULL) {
-    fOutFile->Close();
+    fOutFile -> Close();
     delete fOutFile;
   }
 }
 
-
 void
-STDebugLogger::Hist(TString  name,
-                    Double_t val,
-                    Int_t    nbins,
-                    Double_t min,
-                    Double_t max)
+STDebugLogger::FillHist1(TString name, Double_t val,
+                         Int_t nbins, Double_t min, Double_t max)
 {
-  if(fMapHist[name] == NULL)
+  if (fMapHist1[name] == NULL)
   {
     fOutFile -> cd();
-    TH1D* hist = new TH1D(name,"",nbins,min,max);
-    fMapHist[name] = hist;
+    TH1D* hist = new TH1D(name, "", nbins, min, max);
+    fMapHist1[name] = hist;
   }
-  TH1D* hist = fMapHist[name];
+  TH1D* hist = fMapHist1[name];
   hist -> Fill(val);
 }
+
+void
+STDebugLogger::FillHist1Step(TString name, Double_t val,
+                             Int_t nbins, Double_t min, Double_t max)
+{
+  if (fMapHist1[name] == NULL)
+  {
+    fOutFile -> cd();
+    TH1D* hist = new TH1D(name, "", nbins, min, max);
+    fMapHist1[name] = hist;
+  }
+  TH1D* hist = fMapHist1[name];
+  hist -> Fill(hist -> GetEntries(), val);
+}
+
+void
+STDebugLogger::FillHist2(TString name, Double_t xval, Double_t yval,
+                         Int_t xnbins, Double_t xmin, Double_t xmax,
+                         Int_t ynbins, Double_t ymin, Double_t ymax)
+{
+  if (fMapHist2[name] == NULL)
+  {
+    fOutFile -> cd();
+    TH2D* hist = new TH2D(name, "", xnbins, xmin, xmax, ynbins, ymin, ymax);
+    fMapHist2[name] = hist;
+  }
+
+  TH2D* hist = fMapHist2[name];
+  hist -> Fill(xval, yval);
+}
+
+void 
+STDebugLogger::FillTree(TString name, Int_t nVal, Double_t *val, TString *bname)
+{
+  if (fMapTree[name] == NULL)
+  {
+    fOutFile -> cd();
+    TTree* tree = new TTree(name, "");
+    fMapTree[name] = tree;
+    fMapBranchIdx[name] = fMaxBranchIdx;
+
+    if (bname == NULL)
+    {
+      for (Int_t i=0; i<nVal; i++)
+      {
+        Double_t* branchVal = new Double_t;
+        tree -> Branch(Form("v%d",i), &branchVal);
+        fMapBranchVal.push_back(branchVal);
+
+        fMaxBranchIdx++;
+      }
+    }
+    else
+    {
+      for (Int_t i=0; i<nVal; i++)
+      {
+        Double_t* branchVal = new Double_t;
+        tree -> Branch(bname[i], &branchVal);
+        fMapBranchVal.push_back(branchVal);
+
+        fMaxBranchIdx++;
+      }
+    }
+  }
+
+  Int_t idx = fMapBranchIdx[name];
+  for (Int_t i=0; i<nVal; i++)
+  {
+    Double_t* valP = fMapBranchVal[idx+i];
+    *valP = val[i];
+  }
+  fMapTree[name] -> Fill();
+}
+
+TFile* STDebugLogger::GetOutFile() { return fOutFile; }
+TH1D*  STDebugLogger::GetHist1(TString name) { return fMapHist1[name] != NULL ? fMapHist1[name] : NULL; }
+TH2D*  STDebugLogger::GetHist2(TString name) { return fMapHist2[name] != NULL ? fMapHist2[name] : NULL; }
--- util/STDebugLogger.hh
+++ util/STDebugLogger.hh
@@ -1,36 +1,60 @@
 #ifndef STDEBUGLOGGER
 #define STDEBUGLOGGER
 
+// Debug List
 #define DEBUGRIEMANNCUTS
 
 #include "TFile.h"
 #include "TH1D.h"
+#include "TH2D.h"
+#include "TTree.h"
 
 #include <map> 
-#include <vector>
+#include <vector> 
 #include <iostream>
 
 class STDebugLogger
 {
   public:
+    static STDebugLogger* Create(TString name = "st_debug_logger");
     static STDebugLogger* Instance();
+
     STDebugLogger();
+    STDebugLogger(TString name);
     ~STDebugLogger();
 
     void Write();
-    void Hist(TString  name,
-              Double_t val,
-              Int_t    nbins = 100,
-              Double_t min = 0, 
-              Double_t max = 100);
+
+    void FillHist1(TString name, Double_t val,
+                   Int_t nbins = 100, Double_t min = 0., Double_t max = 100.);
+
+    void FillHist1Step(TString name, Double_t val,
+                       Int_t nbins = 100, Double_t min = 0., Double_t max = 100.);
+
+    void FillHist2(TString name, Double_t xval, Double_t yval,
+                   Int_t xnbins = 100, Double_t xmin = 0., Double_t xmax = 100.,
+                   Int_t ynbins = 100, Double_t ymin = 0., Double_t ymax = 100.);
+
+    void FillTree(TString name, Int_t nVal, Double_t *val, TString *bname = NULL);
+
+    TFile* GetOutFile();
+    TH1D*  GetHist1(TString name);
+    TH2D*  GetHist2(TString name);
 
   private:
     TFile* fOutFile;
-    std::map<TString, TH1D*> fMapHist;
+    std::map<TString, TH1D*> fMapHist1;
+    std::map<TString, TH2D*> fMapHist2;
+
+    std::map<TString, TTree*> fMapTree;
+    std::map<TString, Int_t> fMapBranchIdx;
+    std::vector<Double_t*> fMapBranchVal;
+    Int_t fMaxBranchIdx;
+
 
     static STDebugLogger* fInstance;
 
-  ClassDef(STDebugLogger,0)
+  ClassDef(STDebugLogger, 0)
 };
 
 #endif
