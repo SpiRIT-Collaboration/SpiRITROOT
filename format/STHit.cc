9ef7e61c313e8184381b9bb063f87954efeb3b3e
esteejus <geniejhang@majimak.com>
2015-09-29 23:00:03 +0900

    STHit: Now contains more information. - Row and layer numbers of the highest peak pad - Tb number of hit position - Chi-square and NDF values of hit time fit
    - Position and sigma values are stored in individual variables not packed in TVector3.

--- format/STHit.cc
+++ format/STHit.cc
@@ -9,80 +9,128 @@
 
 STHit::STHit()
 {
-  fTrackID = -1;
-  SetHit(-1, 0, 0, -1000, -1);
-
-  fIsClustered = kFALSE;
-  fClusterID = -1;
-
-  fPosSigma = TVector3(0.8, 0.22, 1.2);
+  Init();
 }
 
 STHit::STHit(Int_t hitID, TVector3 vec, Double_t charge)
 {
-  fTrackID = -1;
+  Init();
+
   SetHit(hitID, vec, charge);
-
-  fIsClustered = kFALSE;
-  fClusterID = -1;
-
-  fPosSigma = TVector3(0.8, 0.22, 1.2);
 }
 
 STHit::STHit(Int_t hitID, Double_t x, Double_t y, Double_t z, Double_t charge)
 {
-  fTrackID = -1;
+  Init();
+
   SetHit(hitID, x, y, z, charge);
-
-  fIsClustered = kFALSE;
-  fClusterID = -1;
-
-  fPosSigma = TVector3(0.8, 0.22, 1.2);
 }
 
 STHit::STHit(STHit *hit)
 {
-  fTrackID = hit -> GetTrackID();
-  SetHit(hit -> GetHitID(), hit -> GetPosition(), hit -> GetCharge());
+  Init();
 
   fIsClustered = hit -> IsClustered();
   fClusterID = hit -> GetClusterID();
+  fTrackID = hit -> GetTrackID();
 
-  fPosSigma = TVector3(0.8, 0.22, 1.2);
+  SetHit(hit -> GetHitID(), hit -> GetPosition(), hit -> GetCharge());
+
+  fDx = hit -> GetDx();
+  fDy = hit -> GetDy();
+  fDz = hit -> GetDz();
+
+  fRow = hit -> GetRow();
+  fLayer = hit -> GetLayer();
+  fTb = hit -> GetTb();
+
+  fChi2 = hit -> GetChi2();
+  fNDF = hit -> GetNDF();
 }
 
 STHit::~STHit()
 {
 }
 
+void STHit::Init()
+{
+  fIsClustered = kFALSE;
+  fHitID = -1;
+  fTrackID = -1;
+  fClusterID = -1;
+
+  SetHit(-1, 0, 0, -1000, -1);
+
+  fDx = 0.8;
+  fDy = 0.22;
+  fDz = 1.2;
+
+  fRow = -1;
+  fLayer = -1;
+  fTb = -1;
+
+  fChi2 = -1;
+  fNDF = 0;
+}
+
 void STHit::SetHit(Int_t hitID, TVector3 vec, Double_t charge) 
 { 
-  fHitID    = hitID; 
-  fPosition = vec; 
-  fCharge   = charge; 
+   fHitID = hitID; 
+       fX = vec.X();
+       fY = vec.Y();
+       fZ = vec.Z();
+  fCharge = charge; 
 }
 
 void STHit::SetHit(Int_t hitID, Double_t x, Double_t y, Double_t z, Double_t charge) 
 { 
-  fHitID    = hitID; 
-  fPosition = TVector3(x, y, z); 
-  fCharge   = charge; 
+   fHitID = hitID; 
+       fX = x;
+       fY = y;
+       fZ = z;
+  fCharge = charge; 
 }
 
 void STHit::SetIsClustered(Bool_t value)                        { fIsClustered = value; }
 void STHit::SetHitID(Int_t hitID)                               { fHitID = hitID; }
 void STHit::SetClusterID(Int_t clusterID)                       { fClusterID = clusterID; fIsClustered = kTRUE; }
 void STHit::SetTrackID(Int_t trackID)                           { fTrackID = trackID; }
-void STHit::SetPosition(TVector3 vec)                           { fPosition = vec; }
-void STHit::SetPosition(Double_t x, Double_t y, Double_t z)     { fPosition = TVector3(x, y, z); }
-void STHit::SetPosSigma(TVector3 vec)                           { fPosSigma = vec; }
-void STHit::SetPosSigma(Double_t dx, Double_t dy, Double_t dz)  { fPosSigma = TVector3(dx, dy, dz); }
+void STHit::SetX(Double_t x)                                    { fX = x; }
+void STHit::SetY(Double_t y)                                    { fY = y; }
+void STHit::SetZ(Double_t z)                                    { fZ = z; }
+void STHit::SetPosition(TVector3 vec)                           { fX = vec.X(); fY = vec.Y(); fZ = vec.Z(); }
+void STHit::SetPosition(Double_t x, Double_t y, Double_t z)     { fX = x; fY = y; fZ = z; }
+void STHit::SetDx(Double_t dx)                                  { fDx = dx; }
+void STHit::SetDy(Double_t dy)                                  { fDy = dy; }
+void STHit::SetDz(Double_t dz)                                  { fDz = dz; }
+void STHit::SetPosSigma(TVector3 vec)                           { fDx = vec.X(); fDy = vec.Y(); fDz = vec.Z(); }
+void STHit::SetPosSigma(Double_t dx, Double_t dy, Double_t dz)  { fDx = dx; fDy = dy; fDz = dz; }
 void STHit::SetCharge(Double_t charge)                          { fCharge = charge; }
+
+void STHit::SetRow(Int_t row)                                   { fRow = row; }
+void STHit::SetLayer(Int_t layer)                               { fLayer = layer; }
+void STHit::SetTb(Double_t tb)                                  { fTb = tb; }
+
+void STHit::SetChi2(Double_t chi2)                              { fChi2 = chi2; }
+void STHit::SetNDF(Int_t ndf)                                   { fNDF = ndf; }
 
    Int_t STHit::GetHitID()     const   { return fHitID; }
    Int_t STHit::GetClusterID() const   { return (fIsClustered ? fClusterID : -1); }
    Int_t STHit::GetTrackID()   const   { return fTrackID; }
-TVector3 STHit::GetPosition()  const   { return fPosition; }
-TVector3 STHit::GetPosSigma()  const   { return fPosSigma; }
+Double_t STHit::GetX()         const   { return fX; }
+Double_t STHit::GetY()         const   { return fY; }
+Double_t STHit::GetZ()         const   { return fZ; }
+TVector3 STHit::GetPosition()  const   { return TVector3(fX, fY, fZ); }
+Double_t STHit::GetDx()        const   { return fDx; }
+Double_t STHit::GetDy()        const   { return fDy; }
+Double_t STHit::GetDz()        const   { return fDz; }
+TVector3 STHit::GetPosSigma()  const   { return TVector3(fDx, fDy, fDz); }
 Double_t STHit::GetCharge()    const   { return fCharge; }
   Bool_t STHit::IsClustered()  const   { return fIsClustered; }
+
+   Int_t STHit::GetRow()       const   { return fRow; }
+   Int_t STHit::GetLayer()     const   { return fLayer; }
+Double_t STHit::GetTb()        const   { return fTb; }
+
+Double_t STHit::GetChi2()      const   { return fChi2; }
+   Int_t STHit::GetNDF()       const   { return fNDF; }
--- format/STHit.hh
+++ format/STHit.hh
@@ -31,30 +31,70 @@ class STHit : public TObject
     void SetHitID(Int_t hitID);                              ///< Hit ID setter
     void SetClusterID(Int_t clusterID);                      ///< Cluster stter
     void SetTrackID(Int_t trackID);                          ///< Track ID setter
+    void SetX(Double_t x);                                   ///< x position setter
+    void SetY(Double_t y);                                   ///< y position setter
+    void SetZ(Double_t z);                                   ///< z position setter
     void SetPosition(TVector3 vec);                          ///< Position setter
     void SetPosition(Double_t x, Double_t y, Double_t z);    ///< Position setter
+    void SetDx(Double_t dx);                                 ///< x position sigma setter
+    void SetDy(Double_t dy);                                 ///< y position sigma setter
+    void SetDz(Double_t dz);                                 ///< z position sigma setter
     void SetPosSigma(TVector3 vec);                          ///< Position sigma setter
     void SetPosSigma(Double_t dx, Double_t dy, Double_t dz); ///< Position sigma setter
     void SetCharge(Double_t charge);                         ///< Charge setter
+
+    void SetRow(Int_t row);                                  ///< Row number setter
+    void SetLayer(Int_t layer);                              ///< Layer number setter
+    void SetTb(Double_t tb);                                 ///< Time bucket number setter
+
+    void SetChi2(Double_t chi2);                             ///< Chi-square setter
+    void SetNDF(Int_t ndf);                                  ///< NDF setter
 
       Bool_t IsClustered()  const; ///< Clustered flag getter
        Int_t GetHitID()     const; ///< Hit ID getter
        Int_t GetClusterID() const; ///< Cluster ID getter
        Int_t GetTrackID()   const; ///< Track ID getter
+    Double_t GetX()         const; ///< x position getter
+    Double_t GetY()         const; ///< y position getter
+    Double_t GetZ()         const; ///< z position getter
     TVector3 GetPosition()  const; ///< Position getter
     TVector3 GetPosSigma()  const; ///< Position sigma getter
+    Double_t GetDx()        const; ///< x position sigma getter
+    Double_t GetDy()        const; ///< y position sigma getter
+    Double_t GetDz()        const; ///< z position sigma getter
     Double_t GetCharge()    const; ///< Charge getter
+
+       Int_t GetRow()       const; ///< Row number getter
+       Int_t GetLayer()     const; ///< Layer number sgtter
+    Double_t GetTb()        const; ///< Time bucket number getter
+
+    Double_t GetChi2()      const; ///< Chi-square getter
+       Int_t GetNDF()       const; ///< NDF getter
 
   protected:
       Bool_t fIsClustered; ///< Clustered flag
        Int_t fHitID;       ///< Hit ID
        Int_t fClusterID;   ///< Cluster ID having this hit
        Int_t fTrackID;     ///< Track ID having this hit
-    TVector3 fPosition;    ///< Position
-    TVector3 fPosSigma;    ///< Position error
+    Double_t fX;           ///< x position
+    Double_t fY;           ///< y position
+    Double_t fZ;           ///< z position
+    Double_t fDx;          ///< x position sigma
+    Double_t fDy;          ///< y position sigma
+    Double_t fDz;          ///< z position sigma
     Double_t fCharge;      ///< Charge
 
-  ClassDef(STHit, 3);
+       Int_t fRow;         ///< Row number of the pad having maximum peak
+       Int_t fLayer;       ///< Layer number of the pad having maximum peak
+    Double_t fTb;          ///< Time bucket number of hit position
+
+    Double_t fChi2;        ///< Chi-square of hit time fit
+       Int_t fNDF;         ///< NDF of hit time fit
+
+  private:
+    void Init();
+
+  ClassDef(STHit, 4);
 };
 
 #endif
