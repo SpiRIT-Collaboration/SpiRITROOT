a6ac42dae0c33eae7253b6b9c27608059d4ed0b3
esteejus <geniejhang@majimak.com>
2015-10-18 04:46:50 +0900

    makeGainCalibraiton.C: Data files before merging can be used for analyzing. createList.sh: Bash script for generating data file list for each voltage.

---
+++ macros/gainCalibrationMacro/createList.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+##########################################################################################
+# Configuration part #####################################################################
+##########################################################################################
+
+# Output list file
+#   If the same file exists, existing file will be deleted.
+OUTPUT=list.txt
+
+# Voltage list separated by a space
+#   Voltage order and the number of voltages should be the same as the file number order.
+VOLTAGES=(5 4 3 2 1)
+
+# Start and end number of the data
+#   It'll loop over the numbers in two numbers
+FIRST=0;
+SECOND=4;
+
+##########################################################################################
+# Don't edit the below ###################################################################
+##########################################################################################
+
+rm -f $OUTPUT
+
+for i in $(seq $FIRST $SECOND);
+do
+  echo ${VOLTAGES[$((i-FIRST))]} >> $PWD/list.txt;
+  if [ $i -le 1000 ];
+  then
+    ls /data/spdaq0?/narval/cobo??/run_0$i* >> $PWD/$OUTPUT
+  else
+    ls /data/spdaq0?/narval/cobo??/run_$i* >> $PWD/$OUTPUT
+  fi
+done
--- macros/gainCalibrationMacro/makeGainCalibration.C
+++ macros/gainCalibrationMacro/makeGainCalibration.C
@@ -11,14 +11,14 @@
 // Put the parameter file name. Path is automatically concatenated.
 TString fParameterFile = "ST.parameters.par";
 
-// Set the directory path where the data files are.
-TString fDataDir = "/path/to/data/dir/";
-
 // Set the output filename with the extension. Checking filename is automatically determined.
 TString fOutputFilename = "";
 
 // FPN pedestal range selection threshold
 Int_t fFPNThreshold = 5;
+
+// Set the directory path where the data files are.
+TString fDataDir = ".";
 
 // Voltage list
 Double_t fVoltage[] = {1.0, 2.0, 3.0, 4.0, 5.0};
@@ -29,6 +29,13 @@ TString fData[] = {"pulser1.dat",
                    "pulser3.dat",
                    "pulser4.dat",
                    "pulser5.dat"};
+
+// Set use separated data files
+Bool_t fUseSeparatedData = kTRUE;
+
+// If set use separated data files, data file list should be set. Voltage list, data file list and data dir above are neglected.
+// Only data used in the list file below.
+TString fDataList = "list.txt";
 
 //////////////////////////////////////////////////////////
 //                                                      //
@@ -41,12 +48,30 @@ void makeGainCalibration() {
   TString parameterDir = workDir + "/parameters/";
 
   STGenerator *fGenerator = new STGenerator("gain");
+  fGenerator -> SetUseSeparatedData(fUseSeparatedData);
   fGenerator -> SetParameterFile(parameterDir + fParameterFile);
   fGenerator -> SetFPNPedestal(fFPNThreshold);
 
-  Int_t numData = (Int_t)(sizeof(fVoltage)/sizeof(Double_t));
-  for (Int_t iData = 0; iData < numData; iData++) 
-    fGenerator -> AddData(fVoltage[iData], fDataDir + "/" + fData[iData]);
+  if (!fUseSeparatedData) {
+    Int_t numData = (Int_t)(sizeof(fVoltage)/sizeof(Double_t));
+    for (Int_t iData = 0; iData < numData; iData++) 
+      fGenerator -> AddData(fVoltage[iData], fDataDir + "/" + fData[iData]);
+  } else {
+    TString fileListWithPath = fDataDir + "/" + fDataList;
+    std::ifstream listFile(fileListWithPath.Data());
+    TString buffer;
+    buffer.ReadLine(listFile);
+    while (!buffer.IsNull()) {
+      Double_t voltage = buffer.Atof();
+
+      for (Int_t iCobo = 0; iCobo < 12; iCobo++) {
+        buffer.ReadLine(listFile);
+        fGenerator -> AddData(voltage, buffer, iCobo);
+      }
+
+      buffer.ReadLine(listFile);
+    }
+  }
 
   fGenerator -> SetOutputFile(fOutputFilename);
 
