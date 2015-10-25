e4c6d080df2b04e8fac32bfb80337ca87ecc9852
Genie Jhang <geniejhang@majimak.com>
2015-10-25 17:29:59 +0900

    createList.sh for all: File ordering is now done correctly.

--- macros/gainCalibrationMacro/createList.sh
+++ macros/gainCalibrationMacro/createList.sh
@@ -6,21 +6,33 @@
 
 if [ -z "$1" ];
 then
-  echo "Please put the run number after the script file."
-  echo "  ex) ./createList.sh 370"
+  echo "== Please put the run number after the script file."
+  echo "==   ex) ./createList.sh 370"
 
   exit 1
 fi
 
 RUN=$1
+printf -v RUN "%04g" $RUN
 
-if [ $RUN -le 1000 ];
-then
-  OUTPUT=list_run0$RUN\.txt
-  > $PWD/$OUTPUT
-  ls /data/spdaq0?/narval/cobo??/run_0$RUN* >> $PWD/$OUTPUT
-else
-  OUTPUT=list_run$RUN\.txt
-  > $PWD/$OUTPUT
-  ls /data/spdaq0?/narval/cobo??/run_$RUN* >> $PWD/$OUTPUT
-fi
+OUTPUT=list_run$RUN\.txt
+> $PWD/$OUTPUT
+
+echo "== Creating list as $OUTPUT. Please be patient."
+
+for COBO in $(seq -f "%02g" 0 11);
+do
+  ls /data/spdaq0?/narval/cobo$COBO/run_$RUN*s >> $OUTPUT
+  for FILENUM in {1..100};
+  do
+    RESULT=`ls /data/spdaq0?/narval/cobo$COBO/run_$RUN*s.$FILENUM 2> /dev/null`
+    if [[ -a $RESULT ]];
+    then
+      echo $RESULT >> $OUTPUT
+    else
+      break
+    fi
+  done
+done
+
+echo "== Done! Enjoy analysis!"
