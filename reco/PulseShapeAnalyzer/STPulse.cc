a1c995171482ece4f1eda6b4f16e84845d0822c8
ejungwoo <phyjics@gmail.com>
2016-02-05 00:24:30 +0900

    STPulse: Generalized. Added comments.

--- reco/PulseShapeAnalyzer/STPulse.cc
+++ reco/PulseShapeAnalyzer/STPulse.cc
@@ -9,32 +9,69 @@ ClassImp(STPulse)
 
 STPulse::STPulse()
 {
-  fPulseData = new Double_t[2000];
-
   TString sysFile = gSystem -> Getenv("VMCWORKDIR");
   TString fileName = sysFile + "/parameters/PulserFitted.dat";
+  Initialize(fileName);
+}
+
+STPulse::STPulse(TString fileName, Double_t step)
+{
+  fStep = step;
+  Initialize(fileName);
+}
+
+void STPulse::Initialize(TString fileName)
+{
   std::ifstream file(fileName);
 
-  Int_t countData = 0;
-  while (file >> fPulseData[countData++]) {}
+  Double_t value;
+  Double_t max = 0;
+
+  while (file >> value) {
+    if (max < value) {
+      max = value; 
+      fTbAtMax = fNumDataPoint * fStep;
+    }
+    fNumDataPoint++;
+  }
+
+  if (fNumDataPoint == 0 || max == 0) {
+    cout << "*** Error occured while initializing the pulse!" << endl;
+    cout << "*** Check the file: " << fileName << endl;
+    return;
+  }
+
+  Double_t c = 1;
+  if (max != 1)
+    c = 1./max;
+
+  fPulseData = new Double_t[fNumDataPoint];
+
+  file.clear();
+  file.seekg(0, ios::beg);
+
+  for(Int_t iData = 0; iData < fNumDataPoint; iData++){
+    file >> value;
+    fPulseData[iData] = c * value;
+  }
 
   file.close();
 }
 
 Double_t 
-STPulse::Pulse(Double_t x, Double_t amp, Double_t tb)
+STPulse::Pulse(Double_t x, Double_t amp, Double_t tb0)
 {
-  if (x < tb) 
+  Double_t tb = x - tb0;
+  if (tb < 0) 
     return 0;
 
-  Double_t hit_time = x - tb;
-  Int_t hit_time_0p1 = hit_time * 10;
+  Int_t tbInStep = tb / fStep;
 
-  if (hit_time_0p1 > 1998) 
+  if (tbInStep > fNumDataPoint - 2) 
     return 0;
 
-  Double_t r = 10 * hit_time - hit_time_0p1;
-  Double_t val = r * fPulseData[hit_time_0p1 + 1] + (1 - r) * fPulseData[hit_time_0p1];
+  Double_t r = (tb / fStep) - tbInStep;
+  Double_t val = r * fPulseData[tbInStep + 1] + (1 - r) * fPulseData[tbInStep];
 
   return amp * val;
 }
@@ -42,18 +79,16 @@ STPulse::Pulse(Double_t x, Double_t amp, Double_t tb)
 Double_t 
 STPulse::PulseF1(Double_t *x, Double_t *par)
 {
-  //x[0] += 0.5;
-
-  if (x[0] < par[1]) 
+  Double_t tb = x[0] - par[1];
+  if (tb < 0) 
     return 0;
 
-  Double_t hit_time = x[0] - par[1];
-  Int_t hit_time_0p1 = hit_time * 10;
-  if (hit_time_0p1 > 1998) 
+  Int_t tbInStep = tb / fStep;
+  if (tbInStep > fNumDataPoint - 2)
     return 0;
 
-  Double_t r = 10 * hit_time - hit_time_0p1;
-  Double_t val = r * fPulseData[hit_time_0p1 + 1] + (1 - r) * fPulseData[hit_time_0p1];
+  Double_t r = (tb / fStep) - tbInStep;
+  Double_t val = r * fPulseData[tbInStep + 1] + (1 - r) * fPulseData[tbInStep];
 
   return par[0] * val;
 }
@@ -61,6 +96,8 @@ STPulse::PulseF1(Double_t *x, Double_t *par)
 TF1*
 STPulse::GetPulseFunction(TString name)
 {
+  if (name.IsNull()) 
+    name = Form("STPulse_%d", fNumF1++);
   TF1* f1 = new TF1(name, this, &STPulse::PulseF1, 0, 512, 2, "STPulse", "PulseF1");
   return f1;
 }
@@ -68,8 +105,6 @@ STPulse::GetPulseFunction(TString name)
 TF1*
 STPulse::GetPulseFunction(STHit* hit, TString name)
 {
-  if (name.IsNull()) 
-    name = Form("STPulse_%d", fIndex++);
   TF1* f1 = GetPulseFunction(name);
   f1 -> SetParameters(hit -> GetCharge(), hit -> GetTb());
   return f1;
--- reco/PulseShapeAnalyzer/STPulse.hh
+++ reco/PulseShapeAnalyzer/STPulse.hh
@@ -8,20 +8,63 @@
 class STPulse
 {
   public:
+    /** Construct STPulse with default setting */
     STPulse();
+
+    /**
+     * Construct STPulse with fileName and step.
+     * step is the difference of the time-bucket between data points.
+     * See fStep.
+     */
+    STPulse(TString fileName, Double_t step = 0.1);
+
     ~STPulse() {}
 
-    Double_t PulseF1(Double_t *x, Double_t *par);
-    Double_t Pulse(Double_t x, Double_t amp, Double_t tb);
+    /** Get value of starting point of the pulse where the peak value is 1 */
+    Double_t GetStartValue() { return fPulseData[0]; }
 
-    TF1* GetPulseFunction(TString name);
+    /** Get time-bucket difference from start of the pulse to the peak */
+    Double_t GetTbAtMax() { return fTbAtMax; }
+
+    /** Get Pulse value in x position with parameter (amp, tb0) */
+    Double_t Pulse(Double_t x, Double_t amp, Double_t tb0);
+
+    /** Get Pulse with inialtial parameter (0,0) */
+    TF1* GetPulseFunction(TString name = "");
+
+    /** Get Pulse with STHit information */
     TF1* GetPulseFunction(STHit* hit, TString name = "");
 
   private:
-    Double_t *fPulseData;
-    Int_t fIndex = 0;
+    /** Initialize data and parameters. */
+    void Initialize(TString fileName);
 
-  ClassDef(STPulse, 1)
+    /** A general C++ function object (functor) with parameters */
+    Double_t PulseF1(Double_t *x, Double_t *par);
+
+    /** Number of data points. Will be updated as STPulse is initialized. */
+    Int_t fNumDataPoint = 0;
+
+    /** The Pulse data points Will be updated as STPulse is initialized. */
+    Double_t *fPulseData;
+
+    /** 
+     * Time-bucket difference from start of the pulse to the peak.
+     * Will be updated as STPulse is initialized.
+     */
+    Double_t fTbAtMax = 0; 
+
+    /** 
+     * Step of data points in 1 time-bucket unit for current data file.
+     * Should be smaller than 1.
+     * The data points are parted by (fStep * [time-bucket]) from each other.
+     */
+    Double_t fStep = 0.1;
+
+    /** Number of the pulse function(TF1*) created by this class */
+    Int_t fNumF1 = 0;
+
+  ClassDef(STPulse, 2)
 };
 
 #endif
