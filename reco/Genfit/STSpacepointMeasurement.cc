26a3a9a10f1a7aaf0616d651ad63f0bbe6b5025b
Genie Jhang <geniejhang@majimak.com>
2016-03-21 03:03:37 +0900

    STSpacepointMeasurement: Temporary covariance matrix values.

--- reco/Genfit/STSpacepointMeasurement.cc
+++ reco/Genfit/STSpacepointMeasurement.cc
@@ -43,7 +43,11 @@ STSpacepointMeasurement::STSpacepointMeasurement(const STHitCluster *detHit, con
   TVector3 posSigma = detHit -> GetPosSigma();
   TMatrixDSym cov(3);
   for (Int_t iComp = 0; iComp < 3; iComp++)
-    cov(iComp, iComp) = posSigma(iComp)*posSigma(iComp)/100.;
+    cov(iComp, iComp) = 0.05*0.05;
+//    cov(iComp, iComp) = posSigma(iComp)*posSigma(iComp);
+//    cov(0, 0) = 0.08*0.08;
+//    cov(1, 1) = 0.02*0.02;
+//    cov(2, 2) = 0.12*0.12;
 
   rawHitCov_ = cov;
   detId_ = hit -> getDetId();
