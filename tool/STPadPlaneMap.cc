1ed0886f5a1004270d2965bf47fabcc848b76ca0
ejungwoo <phyjics@gmail.com>
2016-06-23 20:11:40 +0900

    STPadPlaneMap(event map) update.

--- tool/STPadPlaneMap.cc
+++ tool/STPadPlaneMap.cc
@@ -1,29 +1,60 @@
 #include "STPadPlaneMap.hh"
+#include <iostream>
+using namespace std;
 
-void STPadMap::Clear()
+void STPadHitContainer::Clear()
 {
   fHitArray.clear();
 }
 
-void STPadMap::AddNeighbor(STPadMap *neighbor)
+void STPadHitContainer::AddNeighbor(STPadHitContainer *neighbor)
 {
   fNeighbors.push_back(neighbor);
 }
 
-pads_t *STPadMap::GetNeighbors() { return &fNeighbors; }
+pads_t *STPadHitContainer::GetNeighbors() { return &fNeighbors; }
 
-void STPadMap::AddHit(STHit *hit)
+void STPadHitContainer::AddHit(STHit *hit)
 {
   fHitArray.push_back(hit);
 }
 
-void STPadMap::GetHits(hits_t *array)
+void STPadHitContainer::GetHits(hits_t *array)
 {
-  for (auto *hit : fHitArray)
+  for (auto hit : fHitArray)
     array -> push_back(hit);
 }
 
-void STPadMap::PullOutHits(hits_t *array)
+STHit *STPadHitContainer::PullOutNextHit()
+{
+  if (fHitArray.size() == 0)
+    return nullptr;
+
+  STHit *hit = fHitArray.back();
+  fHitArray.pop_back();
+  return hit;
+}
+
+STHit *STPadHitContainer::PullOutNextFreeHit()
+{
+  Int_t numHits = fHitArray.size();
+  if (numHits == 0)
+    return nullptr;
+
+  for (Int_t i = 0; i < numHits; i++) {
+    Int_t idx = numHits-i-1;
+    STHit *hit = fHitArray[idx];
+    if (hit -> GetNumTrackCands() == 0) {
+      fHitArray.erase(fHitArray.begin()+idx);
+      return hit;
+    }
+  }
+
+  return nullptr;
+}
+
+
+void STPadHitContainer::PullOutHits(hits_t *array)
 {
   auto numHits = fHitArray.size();
   for (Int_t iHit = 0; iHit < numHits; iHit++) 
@@ -40,25 +71,24 @@ STPadPlaneMap::STPadPlaneMap()
 {
   for (Int_t row = 0; row < 108; row++)
     for (Int_t layer = 0; layer < 112; layer++) 
-      fPadPlaneMap[row][layer] = new STPadMap();
+      fPadMap[row][layer] = new STPadHitContainer();
+
+  Int_t drow[]   = {0, 0, 1, -1};
+  Int_t dlayer[] = {-1, 1, 0, 0};
 
   for (Int_t row = 0; row < 108; row++) {
-    for (Int_t layer = 0; layer < 112; layer++) 
-    {
-      Int_t pm[] = {-1, 1};
+    for (Int_t layer = 0; layer < 112; layer++) {
 
-      for (Int_t drow : pm) {
-        for (Int_t dlayer : pm) 
-        {
-          Int_t rowNb = row + drow;
-          Int_t layerNb = row + dlayer;
+      for (Int_t i = 0; i < 4; i++) {
+        Int_t rowNb = row + drow[i];
+        Int_t layerNb = layer + dlayer[i];
 
-          if (rowNb < 0 || rowNb >= 108 || layerNb < 0 || layerNb >= 112 )
-            continue;
+        if (rowNb < 0 || rowNb >= 108 || layerNb < 0 || layerNb >= 112 )
+          continue;
 
-          fPadPlaneMap[row][layer] -> AddNeighbor(fPadPlaneMap[rowNb][layerNb]);
-        }
+        fPadMap[row][layer] -> AddNeighbor(fPadMap[rowNb][layerNb]);
       }
+
     }
   }
 }
@@ -67,23 +97,124 @@ void STPadPlaneMap::Clear()
 {
   for (Int_t row = 0; row < 108; row++)
     for (Int_t layer = 0; layer < 112; layer++) 
-      fPadPlaneMap[row][layer] -> Clear();
+      fPadMap[row][layer] -> Clear();
 
+  fNextRow = 107;
+  fNextLayer = 111;
+  fNextFreeRow = 107;
+  fNextFreeLayer = 111;
 }
 
 void STPadPlaneMap::AddHit(STHit *hit)
 {
-  fPadPlaneMap[hit->GetRow()][hit->GetLayer()] -> AddHit(hit);
+  fPadMap[hit->GetRow()][hit->GetLayer()] -> AddHit(hit);
+}
+
+void STPadPlaneMap::AddHits(hits_t *hits)
+{
+  for (auto hit : *hits)
+    fPadMap[hit->GetRow()][hit->GetLayer()] -> AddHit(hit);
+}
+
+STHit *STPadPlaneMap::PullOutNextHit()
+{
+  if (fNextRow == 0 && fNextLayer == 0)
+    return nullptr;
+
+  STHit *hit = fPadMap[fNextRow][fNextLayer] -> PullOutNextHit();
+
+  if (hit == nullptr) {
+    if (fNextRow == 0) {
+      fNextLayer--;
+      fNextRow = 107;
+    } else
+      fNextRow--;
+
+    return PullOutNextHit();
+  }
+  else
+    return hit;
+}
+
+STHit *STPadPlaneMap::PullOutNextFreeHit()
+{
+  if (fNextFreeRow == 0 && fNextFreeLayer == 0)
+    return nullptr;
+
+  STHit *hit = fPadMap[fNextFreeRow][fNextFreeLayer] -> PullOutNextFreeHit();
+
+  if (hit == nullptr) {
+    if (fNextFreeRow == 0) {
+      fNextFreeLayer--;
+      fNextFreeRow = 107;
+    } else
+      fNextFreeRow--;
+
+    return PullOutNextFreeHit();
+  }
+  else
+    return hit;
+}
+
+void STPadPlaneMap::PullOutNeighborHits(hits_t *hits, hits_t *array, Int_t level)
+{
+  for (auto hit : *hits)
+  {
+    auto pad = fPadMap[hit->GetRow()][hit->GetLayer()];
+    auto neighbors = pad -> GetNeighbors();
+    while (level > 0)
+    {
+      pads_t neighborsSum;
+      for (auto neighbor : *neighbors) {
+        auto neighbors2 = pad -> GetNeighbors();
+        for (auto neighbor2 : *neighbors2)
+          neighborsSum.push_back(neighbor2);
+      }
+      for (auto neighbor2 : neighborsSum)
+        neighbors -> push_back(neighbor2);
+      level--;
+    }
+    for (auto neighbor : *neighbors)
+    {
+      neighbor -> PullOutHits(array);
+    }
+  }
 }
 
 void STPadPlaneMap::PullOutNeighborHits(STHit *hit, hits_t *array)
 {
-  auto *pad = fPadPlaneMap[hit->GetRow()][hit->GetLayer()];
-  auto *neighbors = pad -> GetNeighbors();
+  auto pad = fPadMap[hit->GetRow()][hit->GetLayer()];
+  auto neighbors = pad -> GetNeighbors();
   for (auto neighbor : *neighbors)
   {
     neighbor -> PullOutHits(array);
   }
+}
+
+void STPadPlaneMap::PullOutNeighborHits(TVector3 pos, Double_t range, hits_t *array)
+{
+  Int_t row = CalculateRow(pos.X());
+  Int_t layer = CalculateLayer(pos.Z());
+  Int_t rowRange = (range+4)/8;
+  Int_t layerRange = (range+6)/12;
+
+  for (auto iRow = -rowRange; iRow <= rowRange; iRow++) {
+    for (auto iLayer = -layerRange; iLayer <= layerRange; iLayer++) {
+      Int_t rowNb = row+iRow;
+      Int_t layerNb = layer+iLayer;
+      if (rowNb < 0 || rowNb >= 108 || layerNb < 0 || layerNb >= 112 )
+        continue;
+      fPadMap[rowNb][layerNb] -> PullOutHits(array);
+    }
+  }
+}
+
+void STPadPlaneMap::PullOutPadHits(Int_t row, Int_t layer, hits_t *array)
+{
+  if (row < 0 || row >= 108 || layer< 0 || layer>= 112 )
+    return;
+
+  fPadMap[row][layer] -> PullOutHits(array);
 }
 
 void STPadPlaneMap::PullOutRowHits(Int_t row, hits_t *array)
@@ -92,7 +223,7 @@ void STPadPlaneMap::PullOutRowHits(Int_t row, hits_t *array)
     return;
 
   for (Int_t layer = 0; layer < 112; layer++) 
-    fPadPlaneMap[row][layer] -> PullOutHits(array);
+    fPadMap[row][layer] -> PullOutHits(array);
 }
 
 void STPadPlaneMap::PullOutLayerHits(Int_t layer, hits_t *array)
@@ -101,7 +232,15 @@ void STPadPlaneMap::PullOutLayerHits(Int_t layer, hits_t *array)
     return;
 
   for (Int_t row = 0; row < 108; row++) 
-    fPadPlaneMap[row][layer] -> PullOutHits(array);
+    fPadMap[row][layer] -> PullOutHits(array);
+}
+
+void STPadPlaneMap::GetNeighborHits(STHit *hit, hits_t *array)
+{
+  auto pad = fPadMap[hit->GetRow()][hit->GetLayer()];
+  auto neighbors = pad -> GetNeighbors();
+  for (auto neighbor : *neighbors)
+    neighbor -> GetHits(array);
 }
 
 void STPadPlaneMap::GetRowHits(Int_t row, hits_t *array)
@@ -110,7 +249,7 @@ void STPadPlaneMap::GetRowHits(Int_t row, hits_t *array)
     return;
 
   for (Int_t layer = 0; layer < 112; layer++) 
-    fPadPlaneMap[row][layer] -> GetHits(array);
+    fPadMap[row][layer] -> GetHits(array);
 }
 
 void STPadPlaneMap::GetLayerHits(Int_t layer, hits_t *array)
@@ -119,5 +258,10 @@ void STPadPlaneMap::GetLayerHits(Int_t layer, hits_t *array)
     return;
 
   for (Int_t row = 0; row < 108; row++) 
-    fPadPlaneMap[row][layer] -> GetHits(array);
+    fPadMap[row][layer] -> GetHits(array);
 }
+
+Int_t STPadPlaneMap::CalculateRow(Double_t x)   { return Int_t((x+432)/8); }
+Int_t STPadPlaneMap::CalculateLayer(Double_t z) { return Int_t(z/12); }
+Double_t STPadPlaneMap::CalculateX(Int_t row)   { return (row+0.5)*8.-432.; }
+Double_t STPadPlaneMap::CalculateZ(Int_t layer) { return (layer+0.5)*12.; }
--- tool/STPadPlaneMap.hh
+++ tool/STPadPlaneMap.hh
@@ -4,23 +4,25 @@
 #include "STHit.hh"
 #include <vector>
 
-class STPadMap;
+class STPadHitContainer;
 
-typedef std::vector<STPadMap *> pads_t;
+typedef std::vector<STPadHitContainer *> pads_t;
 typedef std::vector<STHit *> hits_t;
 
-class STPadMap
+class STPadHitContainer
 {
   public:
-    STPadMap() {};
-    ~STPadMap() {};
+    STPadHitContainer() {};
+    ~STPadHitContainer() {};
 
     void Clear();
 
-    void AddNeighbor(STPadMap *pad);
+    void AddNeighbor(STPadHitContainer *pad);
     pads_t *GetNeighbors();
 
     void AddHit(STHit *hit);
+    STHit *PullOutNextHit();
+    STHit *PullOutNextFreeHit();
     void PullOutHits(hits_t *array);
     void GetHits(hits_t *array);
 
@@ -38,16 +40,34 @@ class STPadPlaneMap
     void Clear();
 
     void AddHit(STHit *hit);
-    void PullOutNeighborHits(STHit *hit, hits_t *array);
+    void AddHits(hits_t *hits);
 
+    STHit *PullOutNextHit();
+    STHit *PullOutNextFreeHit();
+    void PullOutNeighborHits(hits_t *hits, hits_t *array, Int_t level = 0);
+    void PullOutNeighborHits(STHit *hit, hits_t *array);
+    void PullOutNeighborHits(TVector3 pos, Double_t range, hits_t *array);
+    void PullOutPadHits(Int_t row, Int_t layer, hits_t *array);
     void PullOutRowHits(Int_t row, hits_t *array);
     void PullOutLayerHits(Int_t layer, hits_t *array);
 
+    void GetNeighborHits(STHit *hit, hits_t *array);
     void GetRowHits(Int_t row, hits_t *array);
     void GetLayerHits(Int_t layer, hits_t *array);
 
+    Int_t CalculateRow(Double_t x);
+    Int_t CalculateLayer(Double_t z);
+    Double_t CalculateX(Int_t row);
+    Double_t CalculateZ(Int_t layer);
+
   private:
-    STPadMap *fPadPlaneMap[108][112];
+    STPadHitContainer *fPadMap[108][112];
+
+    Int_t fNextRow = 107;
+    Int_t fNextLayer = 111;
+
+    Int_t fNextFreeRow = 107;
+    Int_t fNextFreeLayer = 111;
 
   ClassDef(STPadPlaneMap, 1)
 };
