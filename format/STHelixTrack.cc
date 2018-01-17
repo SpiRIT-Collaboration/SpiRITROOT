6c7ea4465dcfa3d82d5d0ae086475ec5728941f6
ejungwoo <phyjics@gmail.com>
2018-01-17 12:04:10 -0500

    fix2: in-plane track sort by distance to vertex

--- format/STHelixTrack.cc
+++ format/STHelixTrack.cc
@@ -168,7 +168,10 @@ void STHelixTrack::DeleteHits()
 
 void STHelixTrack::SortHits(bool increasing)
 {
-  if (increasing) {
+  if (TMath::Abs(YLengthInPeriod()) < 50) {
+    auto sorting = STHitByDistanceTo(fVertexPosition);
+    sort(fMainHits.begin(), fMainHits.end(), sorting);
+  } else if (increasing) {
     auto sorting = STHitSortByIncreasingLength(this);
     sort(fMainHits.begin(), fMainHits.end(), sorting);
   } else {
@@ -252,6 +255,8 @@ void STHelixTrack::SetAlphaTail(Double_t alpha)  { fAlphaTail = alpha; }
 
 void STHelixTrack::DetermineParticleCharge(TVector3 vertex)
 {
+  fVertexPosition = vertex;
+
   Double_t lHead = ExtrapolateToAlpha(fAlphaHead);
   Double_t lTail = ExtrapolateToAlpha(fAlphaTail);
 
