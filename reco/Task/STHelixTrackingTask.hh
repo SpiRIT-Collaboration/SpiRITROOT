da0c1be9a93ef99238e9ef21227666ae25793e06
Genie Jhang <geniejhang@majimak.com>
2018-06-27 13:08:10 -0400

    STHelixTrackingTask:Interface methods are added

--- reco/Task/STHelixTrackingTask.hh
+++ reco/Task/STHelixTrackingTask.hh
@@ -22,7 +22,10 @@ class STHelixTrackingTask : public STRecoTask
     void SetNumTracksLowLimit(Int_t limit);
     void SetClusteringOption(Int_t opt);
     void SetSaturationOption(Int_t opt);
+
     void SetClusterCutLRTB(Double_t left, Double_t right, Double_t top, Double_t bottom);
+    void SetCylinderCut(TVector3 center, Double_t radius, Double_t zLength, Double_t margin);
+    void SetSphereCut(TVector3 center, Double_t radius, Double_t margin);
   
     STHelixTrackFinder *GetTrackFinder();
 
@@ -43,6 +46,12 @@ class STHelixTrackingTask : public STRecoTask
     Double_t fCCRight = -1000;
     Double_t fCCTop = 1000;
     Double_t fCCBottom = -1000;
+
+    TVector3 fCutCenter = TVector3(-9999, -9999, -9999);
+    Double_t fCRadius = -1;
+    Double_t fZLength = -1;
+    Double_t fSRadius = -1;
+    Double_t fCutMargin = -1;
 
   ClassDef(STHelixTrackingTask, 2)
 };
