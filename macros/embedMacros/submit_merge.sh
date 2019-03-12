fc3e255f726eb632afea233c2e33c5a724fcf250
hpcc tsangc <tsangchu@msu.edu>
2019-03-12 11:39:39 -0400

    Adding more files needed for HPCC

---
+++ macros/embedMacros/submit_merge.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=3000
+#SBATCH --time=02:00:00
+#SBATCH --job-name="MERGE"
+
+module load GCCcore/6.4.0
+source ${VMCWORKDIR}/build/config.sh
+export LD_LIBRARY_PATH=${VMCWORKDIR}/macros/lib:$LD_LIBRARY_PATH
+
+OUTPUT=$1
+
+hadd -j 10 $VMCWORKDIR/macros/data/${OUTPUT}.digi.root $VMCWORKDIR/macros/data/${OUTPUT}_SARR_0.digi.root $VMCWORKDIR/macros/data/${OUTPUT}_SARR_1.digi.root $VMCWORKDIR/macros/data/${OUTPUT}_SARR_2.digi.root $VMCWORKDIR/macros/data/${OUTPUT}_SARR_3.digi.root $VMCWORKDIR/macros/data/${OUTPUT}_SARR_4.digi.root $VMCWORKDIR/macros/data/${OUTPUT}_SARR_5.digi.root $VMCWORKDIR/macros/data/${OUTPUT}_SARR_6.digi.root $VMCWORKDIR/macros/data/${OUTPUT}_SARR_7.digi.root $VMCWORKDIR/macros/data/${OUTPUT}_SARR_8.digi.root $VMCWORKDIR/macros/data/${OUTPUT}_SARR_9.digi.root
+
+rm $VMCWORKDIR/macros/data/${OUTPUT}_SARR*.digi.root
