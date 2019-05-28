372196438508c30a6beacdae44626229db928329
hpcc tsangc <tsangchu@msu.edu>
2019-05-28 14:26:52 -0400

    Bug fixed for submit_general_all.sh

--- macros/embedMacros/submit_general_all.sh
+++ macros/embedMacros/submit_general_all.sh
@@ -13,9 +13,9 @@ CLEANUPFILE=${10}
 NTOTAL=${11}
 
 # Creat directory for logging 
-#MCID=$(sbatch --parsable ${MCSUBMITFILE} ${OUTPUT} ${CONFIGLIST} ${CONFIGFILE} ${NTOTAL})
-#MERGEID=$(sbatch --parsable --dependency=afterok:${MCID} --kill-on-invalid-dep=yes embedMacros/submit_merge.sh ${OUTPUT})
+MCID=$(sbatch --parsable ${MCSUBMITFILE} ${OUTPUT} ${CONFIGLIST} ${CONFIGFILE} ${NTOTAL})
+MERGEID=$(sbatch --parsable --dependency=afterok:${MCID} --kill-on-invalid-dep=yes embedMacros/submit_merge.sh ${OUTPUT})
 #sbatch --job-name="CleanupFailSafe" --dependency=afternotok:${MERGEID} --kill-on-invalid-dep=yes ${CLEANUPFILE} ${OUTPUT} ${CONFIGLIST} ${NEXTJOB} ${FISHTANKUSER} ${DESTINATION}
-#RECOID=$(sbatch --parsable --dependency=afterok:${MERGEID} --kill-on-invalid-dep=yes ${RECOFILE} ${OUTPUT} ${NEXTJOB} ${CONFIGLIST} ${MCFILE} ${DESTINATION} ${FISHTANKUSER} ${NTOTAL})
-#sbatch --job-name="Cleanup" --dependency=afterany:${RECOID} --kill-on-invalid-dep=yes ${CLEANUPFILE} ${OUTPUT} ${CONFIGLIST} ${NEXTJOB} ${FISHTANKUSER} ${DESTINATION}
-sbatch ${RECOFILE} ${OUTPUT} ${NEXTJOB} ${CONFIGLIST} ${MCFILE} ${DESTINATION} ${FISHTANKUSER} ${NTOTAL}
+RECOID=$(sbatch --parsable --dependency=afterok:${MERGEID} --kill-on-invalid-dep=yes ${RECOFILE} ${OUTPUT} ${NEXTJOB} ${CONFIGLIST} ${MCFILE} ${DESTINATION} ${FISHTANKUSER} ${NTOTAL})
+sbatch --job-name="Cleanup" --dependency=afterany:${RECOID} --kill-on-invalid-dep=yes ${CLEANUPFILE} ${OUTPUT} ${CONFIGLIST} ${NEXTJOB} ${FISHTANKUSER} ${DESTINATION}
+#sbatch ${RECOFILE} ${OUTPUT} ${NEXTJOB} ${CONFIGLIST} ${MCFILE} ${DESTINATION} ${FISHTANKUSER} ${NTOTAL}
