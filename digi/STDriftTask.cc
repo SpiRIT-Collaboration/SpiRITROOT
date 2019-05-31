51b5277ce8dd439bf08c164f9900e6ea3f8e09c9
esteejus <jdogg0075@gmail.com>
2019-05-31 10:53:05 -0400

    Rensheng fixed bug in STDriftTask for anode wire index. Tommy applies a yoffset shift for the MCTrack in the y direciton

--- digi/STDriftTask.cc
+++ digi/STDriftTask.cc
@@ -176,9 +176,10 @@ STDriftTask::Exec(Option_t* option)
       Double_t dz = dr*TMath::Sin(angle); // displacement in y-direction
       Double_t dt = gRandom->Gaus(0,sigmaL)/fVelDrift; // displacement in time
 
-      Int_t iWire = (Int_t)floor((fMCPoint->GetZ()*10+dz+fZSpacingWire/2)/fZSpacingWire);
-      if(iWire<fIFirstWire) iWire = fIFirstWire;
-      if(iWire>fILastWire)  iWire = fILastWire;
+      Int_t iWire = (Int_t)floor((fMCPoint->GetZ()*10+dz)/fZSpacingWire); //The index of the anode wire is from 0 to 112*3-1
+      if(iWire < 0 || iWire > 112*3-1) continue; //the anode wire number wont change for the SpiRIT experiment
+      //      if(iWire<fIFirstWire) iWire = fIFirstWire;
+      //      if(iWire>fILastWire)  iWire = fILastWire;
       Int_t zWire = iWire*fZSpacingWire+fZOffsetWire;
 
       Int_t index = fElectronArray->GetEntriesFast();
