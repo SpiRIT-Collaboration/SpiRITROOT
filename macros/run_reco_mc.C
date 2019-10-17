c58b47d770dfaa87dfc93c8b9df9b87781912eb7
Chun Yuen Tsang <tsangc@steelhead.nscl.msu.edu>
2019-10-17 18:47:07 -0400

    SpaceCharge class and genfitVA task is added to the file

--- macros/run_reco_mc.C
+++ macros/run_reco_mc.C
@@ -6,7 +6,7 @@ void run_reco_mc
   Int_t fSplitNo = 0,
   Int_t fNumEventsInSplit = 10000,
   Double_t fPSAThreshold = 30,
-  TString fParameterFile = "ST.parameters.Commissioning_201604.par",
+  TString fParameterFile = "ST.parameters.fullmc.par",
   TString fPathToData = ""
 )
 {
@@ -31,8 +31,8 @@ void run_reco_mc
   TString geo = spiritroot+"geometry/geomSpiRIT.man.root";
   TString in1 = fPathToData+fName+".digi.root"; 
   TString in2 = fPathToData+fName+".mc.root"; 
-  TString out = fPathToData+"mc"+sRunNo+"_s"+sSplitNo+".reco."+version+".root";
-  TString log = fPathToData+"mc"+sRunNo+"_s"+sSplitNo+"."+version+".log";
+  TString out = fPathToData+fName+"_s"+sSplitNo+".reco."+version+".root";
+  TString log = fPathToData+fName+"_s"+sSplitNo+"."+version+".log";
 
   FairLogger *logger = FairLogger::GetLogger();
   logger -> SetLogToScreen(true);
@@ -56,19 +56,35 @@ void run_reco_mc
   psa -> SetPersistence(true);
   psa -> SetThreshold(fPSAThreshold);
   psa -> SetLayerCut(-1, 112);
-  psa -> SetPulserData("pulser_117ns.dat");
+  psa -> SetPulserData("pulser_117ns_50tb.dat");
+  psa -> SetPSAPeakFindingOption(1);
   psa -> SetGainMatchingData(spiritroot + "parameters/RelativeGain.list");
+  psa -> SetYOffsets(spiritroot + "parameters/yOffsetCalibration.dat");
   
   auto helix = new STHelixTrackingTask();
   helix -> SetPersistence(true);
   helix -> SetClusterPersistence(true);
   helix -> SetClusterCutLRTB(420, -420, -64, -522);
+  helix -> SetEllipsoidCut(TVector3(0, -260, -11.9084), TVector3(60, 50, 110), 5); // current use
   
 
   auto correct = new STCorrectionTask(); //Correct for saturation   
+
+  auto spaceCharge = new STSpaceChargeCorrectionTask();
+  auto gfBField = STGFBField::GetInstance("samurai_field_map", "A", -0.1794, -20.5502, 58.0526);   
+  spaceCharge -> SetBField(gfBField -> GetFieldMap());
+  spaceCharge -> SetSheetChargeDensity(4e-8);
+  spaceCharge -> SetProjectile(STSpaceCharge::Projectile::Sn132);
+  spaceCharge -> SetElectronDrift(true); 
   
   auto genfitPID = new STGenfitPIDTask();
   genfitPID -> SetPersistence(true);
+
+  auto genfitVA = new STGenfitVATask();
+  genfitVA -> SetPersistence(true);
+  genfitVA -> SetFixedVertex(0.,-213.3,-13.2);
+  genfitVA -> SetUseRave(true);
+  genfitVA -> SetFieldOffset(-0.1794, -20.5502, 58.0526); 
   
   auto mctruth = new STMCTruthTask(true);
 
@@ -76,7 +92,9 @@ void run_reco_mc
   run -> AddTask(psa);
   run -> AddTask(helix);
   run -> AddTask(correct);
+  run -> AddTask(spaceCharge);
   run -> AddTask(genfitPID);
+  run -> AddTask(genfitVA);
   run -> AddTask(mctruth);
 
   auto outFile = FairRootManager::Instance() -> GetOutFile();
@@ -109,5 +127,7 @@ void run_reco_mc
   cout << "Input2 : " << in2 << endl;
   cout << "Output : " << out << endl;
 
+  delete correct;
   gApplication -> Terminate();
+
 }
