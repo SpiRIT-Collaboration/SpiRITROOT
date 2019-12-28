0228d90d793f8873e0d7fe10b7cfda70614580c8
Genie Jhang <geniejhang@majimak.com>
2019-12-27 21:54:21 -0500

    STBeamInfo: Removed unnecessary information
    - STGenfitVATask and STSmallOutputTask are modified according to the change in STBeamInfo.
    - Internal variables of STBeamInfo instance are cleared at the start of every event.

--- format/STBeamInfo.cc
+++ format/STBeamInfo.cc
@@ -1,3 +1,14 @@
 #include "STBeamInfo.hh"
 
 STBeamInfo::STBeamInfo(){}
+
+void STBeamInfo::Clear(Option_t *option) {
+  fBeamAoQ = 0;
+  fBeamZ = 0;
+  fRotationAngleATargetPlane = 0;
+  fRotationAngleBTargetPlane = 0;
+  fXTargetPlane = 0;
+  fYTargetPlane = 0;
+  fBeamEnergyTargetPlane = 0;
+  fBeamVelocityTargetPlane = 0;
+}
