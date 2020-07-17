23f83248f7f8ef7b50fa44064e5cc9c50ce56d78
Chun Yuen Tsang <tsangc@steelhead.nscl.msu.edu>
2020-07-16 20:40:19 -0400

    ERATTask is now able to calculate impact parameter

--- ana/STERATTask.cc
+++ ana/STERATTask.cc
@@ -18,9 +18,12 @@ ClassImp(STERATTask);
 STERATTask::STERATTask()
 {
   fLogger = FairLogger::GetLogger(); 
-  fERAT = new TClonesArray("STVectorF");
-  auto erat = new((*fERAT)[0]) STVectorF;
-  erat -> fElements.push_back(0);
+  fbERat = new STVectorF();
+  fbERat -> fElements.push_back(0);
+  fbMult = new STVectorF();
+  fbMult -> fElements.push_back(0);
+  fERAT = new STVectorF();
+  fERAT -> fElements.push_back(0);
 }
 
 STERATTask::~STERATTask()
@@ -37,6 +40,26 @@ InitStatus STERATTask::Init()
   fCMVector = (TClonesArray*) ioMan -> GetObject("CMVector");
   fProb = (TClonesArray*) ioMan -> GetObject("Prob");
   ioMan -> Register("ERAT", "ST", fERAT, fIsPersistence);
+
+  if(!fImpactParameterFilename.empty())
+  {
+    fLogger -> Info(MESSAGE_ORIGIN, ("Loading impact parameter distribution from " + fImpactParameterFilename).c_str());
+    fImpactParameterFile = new TFile(fImpactParameterFilename.c_str());
+    if(!fImpactParameterFile -> IsOpen())
+    {
+      fLogger -> Info(MESSAGE_ORIGIN, (fImpactParameterFilename + " cannot be opened. Will not calculate impact parameter").c_str());
+      fImpactParameterFile -> Delete();
+      fImpactParameterFile = nullptr;
+    }
+    else
+    {
+      fMultHist = (TH1F*) fImpactParameterFile -> Get("Mult");
+      fERatHist = (TH1F*) fImpactParameterFile -> Get("ERat");
+      fData = (TClonesArray*) ioMan -> GetObject("STData");
+      ioMan -> Register("bERat", "ST", fbERat, fIsPersistence);
+      ioMan -> Register("bMult", "ST", fbMult, fIsPersistence);
+    }
+  }
 
   return kSUCCESS;
 }
@@ -68,6 +91,7 @@ void STERATTask::Exec(Option_t *opt)
       for(int itrack = 0; itrack < ntracks; ++itrack)
       {
         auto& P = static_cast<STVectorVec3*>(fCMVector -> At(i)) -> fElements[itrack];
+        if(P.z() < 0) continue;
         double prob = static_cast<STVectorF*>(fProb -> At(i)) -> fElements[itrack];
         double Ei = sqrt(ParticleMass*ParticleMass + P.Mag2());
         double pt = P.Perp();
@@ -76,5 +100,52 @@ void STERATTask::Exec(Option_t *opt)
         El_expected += prob*pl*pl/(ParticleMass + Ei);
       }
     }
-  static_cast<STVectorF*>(fERAT -> At(0)) -> fElements[0] = Et_expected / El_expected;
+  fERAT -> fElements[0] = Et_expected / El_expected;
+
+  if(fImpactParameterFile)
+  {
+    int mult = 0;
+    auto data = static_cast<STData*>(fData -> At(0));
+    for(int i = 0; i < data -> multiplicity; ++i) 
+      if(data -> recodpoca[i].Mag() < 20) 
+        ++mult;
+    fbMult -> fElements[0] = fMultHist -> Interpolate(mult);
+    fbERat -> fElements[0] = fERatHist -> Interpolate(fERAT -> fElements[0]);
+  }
+}
+
+void STERATTask::SetImpactParameterTable(const std::string& table_filename)
+{
+  fImpactParameterFilename = table_filename;
+}
+
+void STERATTask::CreateImpactParameterTable(const std::string& ana_filename, const std::string& output_filename)
+{
+  const int fMultMin = 0;
+  const int fMultMax = 500;
+  const double fERatMin = 0;
+  const double fERatMax = 5;
+  const int fERatBins = 300;
+
+  TChain chain("cbmsim");
+  chain.Add(ana_filename.c_str());
+
+  TFile output(output_filename.c_str(), "RECREATE");
+  TH1F mult_hist("Mult", "Mult", fMultMax - fMultMin + 1, fMultMin, fMultMax);
+  chain.Project("Mult", "Sum$(recodpoca.Mag() < 20)");
+  auto cumulative = mult_hist.GetCumulative(false);
+  int max = double(cumulative -> GetMaximum());
+  for(int i = 1; i < cumulative -> GetNbinsX(); ++i)
+    cumulative -> SetBinContent(i, sqrt(cumulative -> GetBinContent(i)/max));
+  output.cd();
+  cumulative -> Write("Mult");
+
+  TH1F erat_hist("erat", "erat", fERatBins, fERatMin, fERatMax);
+  chain.Project("erat", "ERAT.fElements");
+  cumulative = erat_hist.GetCumulative(false);
+  max = double(cumulative -> GetMaximum());
+  for(int i = 1; i < cumulative -> GetNbinsX(); ++i)
+    cumulative -> SetBinContent(i, sqrt(cumulative -> GetBinContent(i)/max));
+  output.cd();
+  cumulative -> Write("ERat");
 }
--- ana/STERATTask.hh
+++ ana/STERATTask.hh
@@ -25,6 +25,7 @@
 
 // ROOT classes
 #include "TClonesArray.h"
+#include "TH1.h"
 
 // STL
 #include <vector>
@@ -45,7 +46,9 @@ class STERATTask : public FairTask {
     /// Running the task. This will be called when Run() method invoked from FairRun.
     virtual void Exec(Option_t *opt);
     void SetPersistence(Bool_t value);
+    void SetImpactParameterTable(const std::string& table_filename);
 
+    static void CreateImpactParameterTable(const std::string& ana_filename, const std::string& output_filename);
   private:
     FairLogger *fLogger;                ///< FairLogger singleton
     Bool_t fIsPersistence;              ///< Persistence check variable
@@ -53,9 +56,17 @@ class STERATTask : public FairTask {
     STDigiPar *fPar;                    ///< Parameter read-out class pointer
     TClonesArray *fProb;
     TClonesArray *fCMVector;
-    TClonesArray *fERAT;
+    TClonesArray *fData;
+    STVectorF *fERAT;
+    STVectorF *fbERat; // impact parameter from ERAT
+    STVectorF *fbMult; // impact parameter from multiplicity
 
     const std::vector<int> fSupportedPDG = STAnaParticleDB::SupportedPDG;
+
+    std::string fImpactParameterFilename;
+    TFile *fImpactParameterFile = nullptr;
+    TH1F *fMultHist = nullptr;
+    TH1F *fERatHist = nullptr;
     
   ClassDef(STERATTask, 1);
 };
