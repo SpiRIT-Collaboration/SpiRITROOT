5618ad11542ee69e211f5365288c4ba80852924b
Curtis <cnhunt34@gmail.com>
2024-06-14 07:20:39 -0400

    Added TPC run diagnostics GUI

--- macros/Diagnostics.cpp
+++ macros/Diagnostics.cpp
@@ -13,15 +13,33 @@ STRecoTrack *recoPtr;
 
 const double pi = 3.1415926;
 
-void Diagnostics() {
-  if (!(gSystem -> Getenv("RUN"))) {
-    cout << endl;
-    cout << cYELLOW << "== Usage: " << cNORMAL << "RUN=" << cRED << "####" << cNORMAL << " root Diagnostic.cpp" << endl;
-    cout << endl;
-    gSystem -> Exit(0);
-  }
+void Diagnostics(int runNum = 3154) {
 
-  Int_t runNum = atoi(gSystem -> Getenv("RUN"));
+    TString beamDataPathWithFormat = "/mnt/spirit/rawdata/misc/Frozen_Information_For_SpiRIT_Analysis/Aug2019/BeamData/beam/beam_run%d.ridf.root";
+    TString fBeamData = Form(beamDataPathWithFormat.Data(), runNum);
+
+    Double_t fZ, fAoQ, fBeta37;
+    Double_t fBDC1x, fBDC1y, fBDC2x, fBDC2y, fBDCax, fBDCby;
+
+    auto fBeamFile = new TFile(fBeamData);
+    auto fBeamTree = (TTree *) fBeamFile -> Get("TBeam");
+    fBeamTree -> SetBranchAddress("z", &fZ);
+    fBeamTree -> SetBranchAddress("aoq", &fAoQ);
+    fBeamTree -> SetBranchAddress("beta37", &fBeta37);
+    auto fBDCTree = (TTree *) fBeamFile -> Get("TBDC");
+    fBDCTree -> SetBranchAddress("bdc1x", &fBDC1x);
+    fBDCTree -> SetBranchAddress("bdc1y", &fBDC1y);
+    fBDCTree -> SetBranchAddress("bdc2x", &fBDC2x);
+    fBDCTree -> SetBranchAddress("bdc2y", &fBDC2y);
+    fBDCTree -> SetBranchAddress("bdcax", &fBDCax);
+    fBDCTree -> SetBranchAddress("bdcby", &fBDCby);
+
+    auto fBeamEnergy = new STBeamEnergy();
+    fBeamEnergy -> setBeam(runNum);
+    auto fBDCProjection = new STBDCProjection(TString(gSystem -> Getenv("VMCWORKDIR")) + "/parameters/ReducedBMap.txt");
+    fBDCProjection -> setBeam(runNum);
+
+    auto beamCount = fBeamTree -> GetEntries();
 
     TString filePath = TString::Format("data/run%04d_s0.reco.test.root", runNum);
     tree = new TChain("cbmsim");
@@ -36,6 +54,13 @@ void Diagnostics() {
     TH1D *vtxHistY = new TH1D("vtxHistY", "vtxHistY", 100, -250, -200);
     TH1D *vtxHistZ = new TH1D("vtxHistZ", "vtxHistZ", 100, -100, 100);
 
+    TH1D *bdcHistX = new TH1D("bdcHistX", "bdcHistX", 100, -100, 100);
+    TH1D *bdcHistY = new TH1D("bdcHistY", "bdcHistY", 100, -25, 25);
+    TH1D *bdcHistZ = new TH1D("bdcHistZ", "bdcHistZ", 100, -680, -480);
+
+    TH2D *vtxVbdcX = new TH2D("vtxVbdcX", "vtxVbdcX", 100, -25, 25, 100, -25, 25);
+    TH2D *vtxVbdcY = new TH2D("vtxVbdcY", "vtxVbdcY", 100, -250, -200, 100, -25, 25);
+
     TH2D *pidHist = new TH2D("pidHist", "pidHist", 1000, -1000, 2000, 1000, 0, 1000);
     TH1I *multiplicity = new TH1I("multiplicity", "multiplicity", 100, 0, 100);
     TH2D *katanaPos = new TH2D("katanaPos", "katanaPos", 130, -260, 260, 50, -15, 15);
@@ -44,7 +69,30 @@ void Diagnostics() {
 
     int eventCount = tree->GetEntries();
 
+    cout << "eventCount: " << eventCount << "; beamCount: " << beamCount << endl;
+
     for(int i = 0; i < eventCount; i++) {
+
+        fBeamTree -> GetEntry(i);
+        fBDCTree -> GetEntry(i);
+        fBeamEnergy -> reset(fZ, fAoQ, fBeta37);
+
+        Double_t E1 = fBeamEnergy -> getCorrectedEnergy();
+
+        if (fZ > 0 && fZ < 75 && fAoQ > 1. && fAoQ < 3 && fBDC1x > -999 && fBDC1y > -999 && fBDC2x > -999 && fBDC2y > -999) {
+//          Double_t ProjectedAtZ = -580.4 + vertex -> GetPos().Z();  // mid target = -592.644, start pad plane =-580.4, end of pad plane = 763.6
+            //Double_t ProjectedAtZ = -580.4 + (fPeakZ != -9999 ? fPeakZ : vertex -> GetPos().Z());  // mid target = -592.644, start pad plane =-580.4, end of pad plane = 763.6
+            Double_t ProjectedAtZ = -580.4;  // mid target = -592.644, start pad plane =-580.4, end of pad plane = 763.6
+//          double ProjectedAtZ=-592.644;//////mid target = -592.644, start pad plane =-580.4, end of pad plane = 763.6
+            fBDCProjection -> ProjectParticle(fBDC2x, fBDC2y, -2160., fBDCax, fBDCby, fZ, E1, ProjectedAtZ, fBeamEnergy -> getMass());//-580.4,-583.904
+        }
+
+        //cout << "bdc: (" << fBDCProjection -> getX() << ", " << fBDCProjection -> getY() << ", " << fBDCProjection -> getZ() << ")" << endl;
+        
+        bdcHistX->Fill(fBDCProjection -> getX());
+        bdcHistY->Fill(fBDCProjection -> getY());
+        bdcHistZ->Fill(fBDCProjection -> getZ());
+
         reader->Next();
         auto trackCount = recoReader->Get()->GetEntries();
         multiplicity->Fill(trackCount);
@@ -57,10 +105,13 @@ void Diagnostics() {
         }
         if(vertexPtr != NULL) {
             auto vertex = vertexPtr->GetPos();
-            cout << "vertex: (" << vertex.X() << ", " << vertex.Y() << ", " << vertex.Z() << ")" << endl;
+            //cout << "vertex: (" << vertex.X() << ", " << vertex.Y() << ", " << vertex.Z() << ")" << endl;
             vtxHistX->Fill(vertex.X());
             vtxHistY->Fill(vertex.Y());
             vtxHistZ->Fill(vertex.Z());
+
+            vtxVbdcX->Fill(vertex.X(), fBDCProjection -> getX());
+            vtxVbdcY->Fill(vertex.Y(), fBDCProjection -> getY());
         }
 
         for(int r = 0; r < trackCount; r++) {
@@ -89,7 +140,10 @@ void Diagnostics() {
         }
     }
 
-    
+    //for(int i = 0; i < beamCount; i++) {
+
+    //}
+
     TCanvas *cVX = new TCanvas("cVX", "cVX", 1);
     vtxHistX->Draw();
 
@@ -98,7 +152,21 @@ void Diagnostics() {
 
     TCanvas *cVZ = new TCanvas("cVZ", "cVZ", 1);
     vtxHistZ->Draw();
+
+    TCanvas *cBX = new TCanvas("cBX", "cBX", 1);
+    bdcHistX->Draw();
+
+    TCanvas *cBY = new TCanvas("cBY", "cBY", 1);
+    bdcHistY->Draw();
+
+    TCanvas *cBZ = new TCanvas("cBZ", "cBZ", 1);
+    bdcHistZ->Draw();
     
+    TCanvas *cVBX = new TCanvas("cVBX", "cVBX", 1);
+    vtxVbdcX->Draw("COLZ");
+
+    TCanvas *cVBY = new TCanvas("cVBY", "cVBY", 1);
+    vtxVbdcY->Draw("COLZ");
 
     TCanvas *cPID = new TCanvas("cPID", "cPID", 1);
     pidHist->Draw("COLZ");
---
+++ macros/diagGUI.C
@@ -0,0 +1,272 @@
+#include <TGClient.h>
+#include <TCanvas.h>
+#include <TF1.h>
+#include <TRandom.h>
+#include <TGButton.h>
+#include <TGFrame.h>
+#include <TRootEmbeddedCanvas.h>
+#include <RQ_OBJECT.h>
+
+const double pi = 3.1415926;
+
+class DiagnosticsGUI {
+    RQ_OBJECT("DiagnosticsGUI")
+public:
+    DiagnosticsGUI(const TGWindow *p, UInt_t w, UInt_t h);
+    virtual ~DiagnosticsGUI();
+
+    void SetRunNum() { fRunNum = fRunEnt->GetNumberEntry()->GetIntNumber(); }
+    void ProcessRun();
+
+private:
+    TGMainFrame *fMain;
+    TRootEmbeddedCanvas *fEcanvas[10];
+    TGTab *fTab;
+    TGTextEntry *fRunDisp;
+    TGNumberEntry *fRunEnt;
+
+    Int_t fRunNum = 0;
+
+    TChain *tree = nullptr;
+
+    TTreeReader *reader = nullptr;
+    TTreeReaderValue<TClonesArray> *vertexReader = nullptr;
+    TTreeReaderValue<TClonesArray> *recoReader = nullptr;
+
+    STVertex *vertexPtr;
+    STRecoTrack *recoPtr;
+
+    TH2D *pidHist;
+    TH2D *thetPhi;
+    TH1I *multiplicity;
+
+    TH2D *vtxXY;
+    TH2D *vtxVbdcX;
+    TH2D *vtxVbdcY;
+    TH1D *vtxHistZ;
+
+    void Init();
+    void DrawPID();
+    void DrawTP();
+    void DrawMul();
+    void DrawVtxXY();
+    void DrawVtxZ();
+
+};
+
+DiagnosticsGUI::DiagnosticsGUI(const TGWindow *p, UInt_t w, UInt_t h) {
+    fMain = new TGMainFrame(p, w, h);
+
+    // Make Tabs
+    fTab = new TGTab(fMain, w, h);
+    fTab->AddTab("PID");
+    fTab->AddTab("ThetaPhi");
+    fTab->AddTab("Multiplicity");
+    fTab->AddTab("VertexXY");
+    fTab->AddTab("VertexZ");
+    fMain->AddFrame(fTab, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 10,10,10,1));
+    for(int i = 0; i < 5; i++) {
+        auto name = TString::Format("Ecanvas%d", i);
+        fEcanvas[i] = new TRootEmbeddedCanvas(name, fTab->GetTabContainer(i), 1280, 720);
+        (fTab->GetTabContainer(i))->AddFrame(fEcanvas[i], new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 5, 5, 5, 5));
+    }
+
+    // Make Bottom Frame
+    TGHorizontalFrame *hframe = new TGHorizontalFrame(fMain, 200, 40);
+
+    // Run Input 
+    TGLabel *runLabel = new TGLabel(hframe, "Run Number");
+    hframe->AddFrame(runLabel, new TGLayoutHints(kLHintsCenterX, 5, 5, 3, 4));
+    fRunDisp = new TGTextEntry(hframe);
+    //eventNumber->SetAlignment(kTextRight);
+    fRunDisp->SetEnabled(0);
+    hframe->AddFrame(fRunDisp, new TGLayoutHints(kLHintsCenterX, 5, 5, 3, 4));
+    fRunEnt = new TGNumberEntry(hframe, 0.005, 9, 999, TGNumberFormat::kNESInteger, TGNumberFormat::kNEAPositive, TGNumberFormat::kNELLimitMinMax, 0, 9999);
+    fRunEnt->Connect("ValueSet(Int_t)", "DiagnosticsGUI", this, "SetRunNum()");
+    (fRunEnt->GetNumberEntry())->Connect("ReturnPressed()", "DiagnosticsGUI", this, "SetRunNum()");
+    hframe->AddFrame(fRunEnt, new TGLayoutHints(kLHintsCenterX, 5, 5, 3, 4));
+
+    // Button for processing run
+    TGTextButton *process = new TGTextButton(hframe, "&Process");
+    process->Connect("Clicked()", "DiagnosticsGUI", this, "ProcessRun()");
+    hframe->AddFrame(process, new TGLayoutHints(kLHintsCenterX, 5, 5, 3, 4));
+    // Exit button
+    TGTextButton *exit = new TGTextButton(hframe,"&Exit", "gApplication->Terminate(0)");
+    hframe->AddFrame(exit, new TGLayoutHints(kLHintsCenterX, 5, 5, 3, 4));
+    fMain->AddFrame(hframe, new TGLayoutHints(kLHintsCenterX, 2, 2, 2, 2));
+
+    fMain->SetWindowName("TPC Diagnostics");
+    fMain->MapSubwindows();
+    fMain->Resize(fMain->GetDefaultSize());
+    fMain->MapWindow();
+
+    Init();
+}
+
+DiagnosticsGUI::~DiagnosticsGUI() {
+    fMain->Cleanup();
+    delete fMain;
+}
+
+void DiagnosticsGUI::ProcessRun() {
+    fRunDisp->SetText(TString::Format("%d", fRunNum));
+
+    TString filePath = TString::Format("data/run%04d_s0.reco.test.root", fRunNum);
+    TFile *file = TFile::Open(filePath);
+    if(!file || file->IsZombie()) {
+        auto message = TString::Format("Error: Run %04d has not been unpacked! Unpack before plotting.", fRunNum);
+        new TGMsgBox(gClient->GetRoot(), fMain, "Error", message, kMBIconExclamation, kMBOk);
+        if(file) {
+            file->Close();
+            delete file;
+        }
+        return;
+    }
+    file->Close();
+    delete file;
+
+    if(tree != nullptr) {
+        delete tree;
+        tree = nullptr;
+    }
+    tree = new TChain("cbmsim");
+    tree->Add(filePath);
+
+    if(reader != nullptr) {
+        delete reader;
+        reader = nullptr;
+    }
+    reader = new TTreeReader(tree);
+
+    if(vertexReader != nullptr) {
+        delete vertexReader;
+        vertexReader = nullptr;
+    }
+    vertexReader = new TTreeReaderValue<TClonesArray>(*reader, "STVertex");
+    if(recoReader != nullptr) {
+        delete recoReader;
+        recoReader = nullptr;
+    }
+    recoReader = new TTreeReaderValue<TClonesArray>(*reader, "STRecoTrack");
+
+    int eventCount = tree->GetEntries();
+
+    for(int i = 0; i < eventCount; i++) {
+        reader->Next();
+        auto trackCount = recoReader->Get()->GetEntries();
+        multiplicity->Fill(trackCount);
+        vertexPtr = dynamic_cast<STVertex *>((*vertexReader)->At(0));
+
+        //std::cout << i << std::endl;
+
+        if(vertexPtr == NULL) {
+            cout << "vertexPtr is null" << endl;
+        }
+        if(vertexPtr != NULL) {
+            auto vertex = vertexPtr->GetPos();
+            //cout << "vertex: (" << vertex.X() << ", " << vertex.Y() << ", " << vertex.Z() << ")" << endl;
+
+            vtxXY->Fill(vertex.X(), vertex.Y() + 225);
+            //vtxVbdcX->Fill(vertex.X(), fBDCProjection -> getX());
+            //vtxVbdcY->Fill(vertex.Y(), fBDCProjection -> getY());
+            vtxHistZ->Fill(vertex.Z());
+        }
+
+        for(int r = 0; r < trackCount; r++) {
+            recoPtr = dynamic_cast<STRecoTrack *>((*recoReader)->At(r));
+
+            if(recoPtr == NULL) {
+                cout << "recoPtr is null" << endl;
+            }
+            if(recoPtr != NULL) {
+                auto mom = recoPtr->GetMomentum().Mag();
+                auto dedx = recoPtr->GetdEdxWithCut(0, 0.7, 0.5);
+
+                pidHist->Fill(mom, dedx);
+
+                auto pos = recoPtr->GetPosKatana();
+                //katanaPos->Fill(pos.X(), pos.Y());
+                //cout << "track: " << r << "; mom: " << mom << "; dEdx: " << dedx << endl;
+
+                auto momTar = recoPtr->GetMomentumTargetPlane();
+
+                auto theta = momTar.Theta();
+                auto phi = momTar.Phi();
+
+                thetPhi->Fill(theta / pi * 180, (phi + pi) / pi * 180);
+            }
+        }
+    }
+    DrawPID();
+    DrawTP();
+    DrawMul();
+    DrawVtxXY();
+    DrawVtxZ();
+}
+
+void DiagnosticsGUI::Init() {
+
+    pidHist = new TH2D("pidHist", "pidHist", 1000, -1000, 2000, 1000, 0, 1000);
+    thetPhi = new TH2D("thetPhi", "thetPhi", 100, 0, 90, 100, 0, 360);
+    multiplicity = new TH1I("multiplicity", "multiplicity", 100, 0, 100);
+
+    vtxXY = new TH2D("vtxXY", "vtxXY", 100, -25, 25, 100, -25, 25);
+    vtxVbdcX = new TH2D("vtxVbdcX", "vtxVbdcX", 100, -25, 25, 100, -25, 25);
+    vtxVbdcY = new TH2D("vtxVbdcY", "vtxVbdcY", 100, -25, 25, 100, -25, 25);
+    vtxHistZ = new TH1D("vtxHistZ", "vtxHistZ", 100, -100, 100);
+}
+
+void DiagnosticsGUI::DrawPID() {
+    std::cout << "Drawing PID" << endl;
+    TCanvas *canvas = fEcanvas[0]->GetCanvas();
+    canvas->Clear();
+    canvas->cd();
+    pidHist->Draw("COLZ");
+    canvas->Update();
+}
+
+void DiagnosticsGUI::DrawTP() {
+    TCanvas *canvas = fEcanvas[1]->GetCanvas();
+    canvas->Clear();
+    canvas->cd();
+    thetPhi->Draw("COLZ");
+    canvas->Update();
+}
+
+void DiagnosticsGUI::DrawMul() {
+    TCanvas *canvas = fEcanvas[2]->GetCanvas();
+    canvas->Clear();
+    canvas->cd();
+    multiplicity->Draw();
+    canvas->Update();
+}
+
+void DiagnosticsGUI::DrawVtxXY() {
+    TCanvas *canvas = fEcanvas[3]->GetCanvas();
+    canvas->Clear();
+    canvas->Divide(2,2);
+    canvas->cd(1);
+    vtxXY->Draw("COLZ");
+    gPad->Modified();
+    canvas->Update();
+    canvas->cd();
+    canvas->Update();
+}
+
+void DiagnosticsGUI::DrawVtxZ() {
+    TCanvas *canvas = fEcanvas[4]->GetCanvas();
+    canvas->Clear();
+    canvas->cd();
+    vtxHistZ->Draw();
+    canvas->Update();
+}
+
+
+
+void diagGUI() {
+    //TApplication theApp("App", 0, 0);
+
+    new DiagnosticsGUI(gClient->GetRoot(), 200, 200);
+
+    //theApp.Run();
+}
